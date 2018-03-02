7189e89284dd8a59748e6612cd344baf41e7bb73
Julian Pistorius <julian@julianpistorius.com>
2018-03-01 18:32:04 -0700

    Problem: UI looks fugly
    Solution: Use the `avh4/elm-beautiful-example` package until we build a
    nicer version using StyleElements.

--- src/Exosphere.elm
+++ src/Exosphere.elm
@@ -1,6 +1,7 @@
 module Main exposing (main)
 
-import Html exposing (program)
+import BeautifulExample
+import Color
 import State
 import Types.Types exposing (Model, Msg)
 import View
@@ -11,7 +12,15 @@ import View
 
 main : Program Never Model Msg
 main =
-    program
+    BeautifulExample.program
+        { title = "Exosphere"
+        , details =
+            Just """User-friendly, extensible client for cloud computing. Currently targeting OpenStack."""
+        , color = Just Color.blue
+        , maxWidth = 640
+        , githubUrl = Just "https://github.com/exosphere-project/exosphere"
+        , documentationUrl = Just "https://github.com/exosphere-project/exosphere"
+        }
         { init = State.init
         , view = View.view
         , update = State.update
