511880d73dc6c176cdb821c4a7cb2ae63f519ab9
Julian Pistorius <julian@julianpistorius.com>
2019-12-04 17:03:45 -0700

    - Decode & store the ID of the user that created a server

--- src/Rest/Rest.elm
+++ src/Rest/Rest.elm
@@ -1707,6 +1707,7 @@ decodeServerDetails =
         |> Pipeline.optional "key_name" (Decode.string |> Decode.andThen (\s -> Decode.succeed <| Just s)) Nothing
         |> Pipeline.optional "addresses" (Decode.map flattenAddressesObject (Decode.keyValuePairs (Decode.list serverIpAddressDecoder))) []
         |> Pipeline.required "metadata" metadataDecoder
+        |> Pipeline.required "user_id" Decode.string
         |> Pipeline.required "os-extended-volumes:volumes_attached" (Decode.list (Decode.at [ "id" ] Decode.string))
 
 
