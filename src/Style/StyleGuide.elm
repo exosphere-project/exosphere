b4c780cca713a65cbd9b4256b0bf051be4f6b090
Julian Pistorius <julian@julianpistorius.com>
2020-05-29 11:43:00 -0700

    Rough WIP for integrating elm-ui-widgets into style guide.

--- src/Style/StyleGuide.elm
+++ src/Style/StyleGuide.elm
@@ -1,16 +1,21 @@
 module Style.StyleGuide exposing (main)
 
+import Browser
 import Color
 import Element
 import Element.Font as Font
 import Element.Region as Region
 import Framework.Modifier exposing (Modifier(..))
 import Html
+import Set exposing (Set)
 import Style.Widgets.Card exposing (badge, exoCard)
 import Style.Widgets.CopyableText exposing (copyableText)
 import Style.Widgets.Icon exposing (bell, question, remove, roundRect, timesCircle)
 import Style.Widgets.IconButton exposing (chip, iconButton)
 import Style.Widgets.MenuItem exposing (MenuItemState(..), menuItem)
+import Widget
+import Widget.Style exposing (ColumnStyle, TextInputStyle)
+import Widget.Style.Material as Material
 
 
 
@@ -62,8 +67,8 @@ intro =
     ]
 
 
-main : Html.Html msg
-main =
+oldMain : Html.Html msg
+oldMain =
     Element.layout [] <|
         Element.column
             [ Element.padding 10
@@ -74,3 +79,140 @@ main =
                 [ intro
                 , widgets
                 ]
+
+
+
+-- Playing with elm-ui-widgets below
+
+
+type alias Style style msg =
+    { style
+        | textInput : TextInputStyle msg
+        , column : ColumnStyle msg
+    }
+
+
+materialStyle : Style {} msg
+materialStyle =
+    { textInput = Material.textInput Material.defaultPalette
+    , column = Material.column
+    }
+
+
+options : List String
+options =
+    [ "Apple"
+    , "Kiwi"
+    , "Strawberry"
+    , "Pineapple"
+    , "Mango"
+    , "Grapes"
+    , "Watermelon"
+    , "Orange"
+    , "Lemon"
+    , "Blueberry"
+    , "Grapefruit"
+    , "Coconut"
+    , "Cherry"
+    , "Banana"
+    ]
+
+
+type alias Model =
+    { selected : Set String
+    , textInput : String
+    }
+
+
+type Msg
+    = ToggleSelection String
+    | SetTextInput String
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { selected = Set.empty
+      , textInput = ""
+      }
+    , Cmd.none
+    )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ToggleSelection string ->
+            ( { model
+                | selected =
+                    model.selected
+                        |> (if model.selected |> Set.member string then
+                                Set.remove string
+
+                            else
+                                Set.insert string
+                           )
+              }
+            , Cmd.none
+            )
+
+        SetTextInput string ->
+            ( { model | textInput = string }, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+view : (Msg -> msg) -> Style style msg -> Model -> Element.Element msg
+view msgMapper style model =
+    [ { chips =
+            model.selected
+                |> Set.toList
+                |> List.map
+                    (\string ->
+                        { icon = Element.none
+                        , text = string
+                        , onPress =
+                            string
+                                |> ToggleSelection
+                                |> msgMapper
+                                |> Just
+                        }
+                    )
+      , text = model.textInput
+      , placeholder = Nothing
+      , label = "Chips"
+      , onChange = SetTextInput >> msgMapper
+      }
+        |> Widget.textInput style.textInput
+    , model.selected
+        |> Set.diff
+            (options |> Set.fromList)
+        |> Set.filter (String.toUpper >> String.contains (model.textInput |> String.toUpper))
+        |> Set.toList
+        |> List.map
+            (\string ->
+                Widget.button style.textInput.chipButton
+                    { onPress =
+                        string
+                            |> ToggleSelection
+                            |> msgMapper
+                            |> Just
+                    , text = string
+                    , icon = Element.none
+                    }
+            )
+        |> Element.wrappedRow [ Element.spacing 10 ]
+    ]
+        |> Widget.column style.column
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = always init
+        , view = view identity materialStyle >> Element.layout []
+        , update = update
+        , subscriptions = subscriptions
+        }
