022e928f97a46ad87f4467109598327efe2c61b7
chris <chris@c-mart.in>
2020-09-07 16:02:31 -0700

    Add type declaration in Types.Defaults

--- src/Types/Defaults.elm
+++ src/Types/Defaults.elm
@@ -1,6 +1,9 @@
 module Types.Defaults exposing (serverListViewParams)
 
+import Types.Types as Types
 
+
+serverListViewParams : Types.ServerListViewParams
 serverListViewParams =
     { onlyOwnServers = True
     , deleteConfirmations = []
