88d38b407cf2bb0976721c8c4163dcf99720f812
chris <chris@c-mart.in>
2020-09-14 17:47:08 -0700

    simple Python script to output system resource usage

---
+++ scripts/system_load_json.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+# Outputs system resource usage on one line as JSON.
+# Uses only the standard library shipped with CentOS 8 and Ubuntu 20 cloud images.
+# Requires "top", "free", and "df" installed.
+
+import json
+import subprocess
+import sys
+
+
+def cmd_stdout_lines(cmd_list):
+    return subprocess.run(cmd_list, stdout=subprocess.PIPE) \
+        .stdout \
+        .decode() \
+        .split("\n")
+
+
+def cpu():
+    # Returns percent CPU non-idle
+    top_output_lines = cmd_stdout_lines(["top", "-b", "-n", "1"])
+    cpu_line = \
+        next(
+            filter(
+                (lambda l: l[:4] == "%Cpu"),
+                top_output_lines)
+            )
+    cpu_idle_pct = float(cpu_line.split(",")[3][:5])
+    cpu_used_pct = round(100 - cpu_idle_pct)
+    return cpu_used_pct
+
+
+def mem():
+    # Returns percent of total memory not available
+    # https://www.linuxatemyram.com/
+    free_output_lines = cmd_stdout_lines(["free", "-t", "-m"])
+    total_line = \
+        next(
+            filter(
+                (lambda l: l[:4] == "Mem:"),
+                free_output_lines)
+        )
+    mem_avail_mb = int(total_line.split()[6])
+    mem_total_mb = int(total_line.split()[1])
+    mem_unavail_pct = round(100 - mem_avail_mb / mem_total_mb * 100)
+    return mem_unavail_pct
+
+
+def disk():
+    # Returns percent of root filesystem used
+    df_output_lines = cmd_stdout_lines(["df", "/"])
+    rootfs_line = \
+        next(
+            filter(
+                (lambda l: l.split()[-1] == "/"),
+                df_output_lines
+            )
+        )
+    rootfs_used_pct = rootfs_line.split()[-2][:-1]
+    return int(rootfs_used_pct)
+
+
+json_dict = {
+        "cpuPctUsed:": cpu(),
+        "memPctUsed:": mem(),
+        "rootfsPctUsed:": disk()
+    }
+output_line = json.dumps(json_dict) + "\n"
+sys.stdout.buffer.write(output_line.encode())
