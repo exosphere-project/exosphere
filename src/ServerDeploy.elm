2f2104c1ebb294dbb9fd053bdd3cae799ad60eeb
chris <chris@c-mart.in>
2020-10-30 17:08:25 -0700

    CentOS support for Guacamole

--- src/ServerDeploy.elm
+++ src/ServerDeploy.elm
@@ -76,9 +76,9 @@ mounts:
 guacamoleUserData : String
 guacamoleUserData =
     """cd /opt
-    GUAC_CONFIG_GIT_TAG=0
-    GUAC_CONFIG_URL=https://gitlab.com/exosphere/guacamole-config/-/archive/0/guacamole-config-$GUAC_CONFIG_GIT_TAG.tar.gz
-    GUAC_CONFIG_SHA512=00ccf4d00e43f345b97d95a1851a369985af0bd831744b3cf329cb3e9f1402ebdb229af2105e80d91aacc6e73176db7c95d3abfa09e1297708093d82890467d5
+    GUAC_CONFIG_GIT_TAG=1
+    GUAC_CONFIG_URL=https://gitlab.com/exosphere/guacamole-config/-/archive/$GUAC_CONFIG_GIT_TAG/guacamole-config-$GUAC_CONFIG_GIT_TAG.tar.gz
+    GUAC_CONFIG_SHA512=05ffc54514310a7e4a5cfc78e3b3dc6e2dc8090cea8f68ccd022d57a45b3ba81b307692d34171a7370ff080f61282c10dd55034c1d314e8a38bebb2a5c2532a5
     wget --quiet --output-document=guacamole-config.tar.gz $GUAC_CONFIG_URL
     if echo $GUAC_CONFIG_SHA512 guacamole-config.tar.gz | sha512sum --check --quiet; then
       tar -zxvf guacamole-config.tar.gz
