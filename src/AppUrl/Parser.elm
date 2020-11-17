141e9b2b3295099c440c026d701a1e5ad54be112
chris <chris@c-mart.in>
2020-11-17 16:32:59 -0700

    Learned to parse query strings! Parsing URLs for Jetstream and OpenStack login pages

--- src/AppUrl/Parser.elm
+++ src/AppUrl/Parser.elm
@@ -1,10 +1,11 @@
 module AppUrl.Parser exposing (urlToViewState)
 
+import Dict
 import OpenStack.Types as OSTypes
-import Types.Defaults as DefaultTypes
 import Types.Types
     exposing
-        ( JetstreamProvider(..)
+        ( JetstreamCreds
+        , JetstreamProvider(..)
         , Model
         , NonProjectViewConstructor(..)
         , ProjectIdentifier
@@ -12,25 +13,76 @@ import Types.Types
         , ViewState(..)
         )
 import Url
-import Url.Parser as P exposing (..)
+import Url.Parser exposing (..)
+import Url.Parser.Query as Query
 
 
 urlToViewState : Url.Url -> Maybe ViewState
 urlToViewState url =
-    -- TODO parser will need to know path prefix on try, try-dev, etc
     let
         pathParsers =
             [ map
-                -- TODO parse query string here
-                (NonProjectView <| LoginOpenstack <| OSTypes.OpenstackLogin "" "" "" "" "" "")
-                (s "login" </> s "openstack")
+                (\creds -> NonProjectView <| LoginOpenstack creds)
+                (let
+                    queryParser =
+                        Query.map6
+                            OSTypes.OpenstackLogin
+                            (Query.string "authurl"
+                                |> Query.map (Maybe.withDefault "")
+                            )
+                            (Query.string "pdomain"
+                                |> Query.map (Maybe.withDefault "")
+                            )
+                            (Query.string "pname"
+                                |> Query.map (Maybe.withDefault "")
+                            )
+                            (Query.string "udomain"
+                                |> Query.map (Maybe.withDefault "")
+                            )
+                            (Query.string "uname"
+                                |> Query.map (Maybe.withDefault "")
+                            )
+                            -- This parses into a blank password, ugly I know
+                            (Query.string ""
+                                |> Query.map (\_ -> "")
+                            )
+                 in
+                 s "login" </> s "openstack" <?> queryParser
+                )
             , map
-                -- TODO parse query string here
-                (NonProjectView <| LoginJetstream <| DefaultTypes.jetstreamCreds)
-                (s "login" </> s "jetstream")
+                (\creds -> NonProjectView <| LoginJetstream creds)
+                (let
+                    providerEnumDict =
+                        Dict.fromList
+                            [ ( "iu", IUCloud )
+                            , ( "tacc", TACCCloud )
+                            , ( "both", BothJetstreamClouds )
+                            ]
+
+                    queryParser =
+                        Query.map4
+                            JetstreamCreds
+                            (Query.enum "provider" providerEnumDict
+                                |> Query.map (Maybe.withDefault BothJetstreamClouds)
+                            )
+                            (Query.string "pname"
+                                |> Query.map (Maybe.withDefault "")
+                            )
+                            (Query.string "taccuname"
+                                |> Query.map (Maybe.withDefault "")
+                            )
+                            -- This parses into a blank password, ugly I know
+                            (Query.string ""
+                                |> Query.map (\_ -> "")
+                            )
+                 in
+                 s "login" </> s "jetstream" <?> queryParser
+                )
             , map
                 (NonProjectView LoginPicker)
                 (s "login")
+
+            -- Not bothering to decode the SelectProjects view, because you can't currently navigate there on a fresh page load and see anything useful
             ]
     in
     parse (oneOf pathParsers) url
