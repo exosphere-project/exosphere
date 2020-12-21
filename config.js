1aab03d63f47cf923768e2d4e4e1e0449caffb4a
chris <chris@c-mart.in>
2020-12-21 14:35:41 -0700

    Store deployer-modifiable flags in config.js instead of ports.js

---
+++ config.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var config = {
+// These are configuration values that allow Exosphere to be customized for a specific organization or use case.
+// See README.me for documentation on how to use them.
+showDebugMsgs : false,
+cloudCorsProxyUrl: "https://try-dev.exosphere.app/proxy",
+cloudsWithUserAppProxy:
+[ ["iu.jetstream-cloud.org", "proxy-j7m-iu.exosphere.app"],
+  ["tacc.jetstream-cloud.org", "proxy-j7m-tacc.exosphere.app"],
+],
+urlPathPrefix: null,
+palette: null,
+logo: null,
+favicon: null,
+appTitle: null,
+defaultLoginView: null,
+aboutAppMarkdown: null,
+}
