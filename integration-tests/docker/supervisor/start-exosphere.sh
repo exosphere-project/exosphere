d09235c9afc0bd193ca8cabd4cb6db520936fe7c
Julian Pistorius <julian@julianpistorius.com>
2021-01-26 07:42:09 -0700

    Make sure that the Exosphere service starts
    Also make sure that it restarts automatically.

--- integration-tests/docker/supervisor/start-exosphere.sh
+++ integration-tests/docker/supervisor/start-exosphere.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd /usr/src/app
+cd /usr/src/app || exit
 npx elm-live src/Exosphere.elm \
   --proxy-prefix '/proxy' --proxy-host 'https://try-dev.exosphere.app/proxy' \
   --host 0.0.0.0 --start-page index.html \
