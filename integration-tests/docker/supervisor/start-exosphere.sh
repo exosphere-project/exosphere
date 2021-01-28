5b1fd5ecbc231f19ef0e7dae04db59ad9d36de7e
Julian Pistorius <julian@julianpistorius.com>
2021-01-28 13:01:29 -0700

    Use http-server instead of elm-live to serve app

--- integration-tests/docker/supervisor/start-exosphere.sh
+++ integration-tests/docker/supervisor/start-exosphere.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
 cd /usr/src/app || exit
-npx elm-live src/Exosphere.elm \
-  --proxy-prefix '/proxy' --proxy-host 'https://try-dev.exosphere.app/proxy' \
-  --host 0.0.0.0 --start-page index.html \
-  -- --output=elm-web.js
+npx http-server --port 8000
