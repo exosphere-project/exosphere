bef35d75539c5fac64485ba5a3c9264be3251374
chris <chris@c-mart.in>
2021-02-10 12:14:58 -0700

    Attempt at a minimal service worker that satisfies Chrome

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+// Minimal service worker per
+// https://stackoverflow.com/questions/61208907/pwa-minimal-service-worker-just-for-trigger-the-install-button
+// Not intended to do anything useful yet, just enable Chrome/Chromium's "add to home screen" prompt
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
+});
