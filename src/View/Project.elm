694769f5326170a55dca8976a3348b421ca1a079
Julian Pistorius <julian@julianpistorius.com>
2021-02-25 14:55:12 -0700

    Don't use default image search in search box

--- src/View/Project.elm
+++ src/View/Project.elm
@@ -76,13 +76,13 @@ project model palette p viewParams viewConstructor =
         (Element.width Element.fill
             :: VH.exoColumnAttributes
         )
-        [ projectNav palette p viewParams model.style.defaultImageSearchText
+        [ projectNav palette p viewParams
         , v
         ]
 
 
-projectNav : Style.Types.ExoPalette -> Project -> ProjectViewParams -> Maybe String -> Element.Element Msg
-projectNav palette p viewParams maybeImageSearchText =
+projectNav : Style.Types.ExoPalette -> Project -> ProjectViewParams -> Element.Element Msg
+projectNav palette p viewParams =
     Element.column [ Element.width Element.fill, Element.spacing 10 ]
         [ Element.el
             VH.heading2
@@ -134,13 +134,13 @@ projectNav palette p viewParams maybeImageSearchText =
                     }
             , Element.el
                 [ Element.alignRight ]
-                (createButton palette p.auth.project.uuid viewParams.createPopup maybeImageSearchText)
+                (createButton palette p.auth.project.uuid viewParams.createPopup)
             ]
         ]
 
 
-createButton : Style.Types.ExoPalette -> ProjectIdentifier -> Bool -> Maybe String -> Element.Element Msg
-createButton palette projectId expanded maybeImageSearchText =
+createButton : Style.Types.ExoPalette -> ProjectIdentifier -> Bool -> Element.Element Msg
+createButton palette projectId expanded =
     let
         ( attribs, icon ) =
             if expanded then
@@ -162,7 +162,7 @@ createButton palette projectId expanded maybeImageSearchText =
                                         ProjectMsg projectId <|
                                             SetProjectView <|
                                                 ListImages
-                                                    { searchText = Maybe.withDefault "" maybeImageSearchText
+                                                    { searchText = ""
                                                     , tags = Set.empty
                                                     , onlyOwnImages = False
                                                     , expandImageDetails = Set.empty
