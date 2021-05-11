7eb92105e2bc6854941c3e1d138907cc0b27a123
chris <chris@c-mart.in>
2021-05-11 11:00:00 -0700

    more plumbing for floating IPs view

--- src/View/FloatingIps.elm
+++ src/View/FloatingIps.elm
@@ -1,10 +1,29 @@
 module View.FloatingIps exposing (floatingIps)
 
 import Element exposing (Element)
+import Helpers.String
 import Types.Types exposing (FloatingIpListViewParams, Msg(..), Project)
+import View.Helpers as VH
 import View.Types
 
 
 floatingIps : View.Types.Context -> Bool -> Project -> FloatingIpListViewParams -> (FloatingIpListViewParams -> Msg) -> Element.Element Msg
 floatingIps context showHeading project viewParams toMsg =
-    Element.text "TODO"
+    let
+        renderFloatingIps =
+            Element.text "TODO"
+    in
+    Element.column
+        [ Element.spacing 20, Element.width Element.fill ]
+        [ if showHeading then
+            Element.el VH.heading2 <|
+                Element.text
+                    (context.localization.pkiPublicKeyForSsh
+                        |> Helpers.String.pluralize
+                        |> Helpers.String.toTitleCase
+                    )
+
+          else
+            Element.none
+        , Element.text <| Debug.toString project.floatingIps
+        ]
