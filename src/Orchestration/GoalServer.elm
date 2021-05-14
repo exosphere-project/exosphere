eda85299a40c1f8c27e1645765b33af076395f29
chris <chris@c-mart.in>
2021-05-14 13:39:34 -0700

    Using factored-out helper function

--- src/Orchestration/GoalServer.elm
+++ src/Orchestration/GoalServer.elm
@@ -242,10 +242,7 @@ stepServerRequestFloatingIp _ project server =
                             == Requestable
                        )
             then
-                -- TODO use GetterSetters.getServerPort
-                RDPP.withDefault [] project.ports
-                    |> List.filter (\port_ -> port_.deviceUuid == server.osProps.uuid)
-                    |> List.head
+                GetterSetters.getServerPort project server
 
             else
                 Nothing
