140043addbd2bf2279125d6f974d572866072325
chris <chris@c-mart.in>
2021-07-07 14:24:44 -0700

    Don't show bottom border for project heading

--- src/View/Project.elm
+++ src/View/Project.elm
@@ -150,7 +150,7 @@ projectNav context p viewParams =
     in
     Element.row [ Element.width Element.fill, Element.spacing 10, Element.paddingEach { edges | bottom = 10 } ]
         [ Element.el
-            (VH.heading2 context.palette)
+            (VH.heading2 context.palette ++ [ Border.width 0 ])
           <|
             Element.text <|
                 UrlHelpers.hostnameFromUrl p.endpoints.keystone
