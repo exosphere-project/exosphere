exosphere-project/exosphere

commit 5d5aa4f239d2b4ac620d570fa1f71289a7f259ae

moving ViewState to OuterModel to avoid future circular imports. this created lots of temporary code that will be cleaned up later.

src/State/Init.elm
@@ -21,6 +21,7 @@ import Toasty
 import Types.Defaults as Defaults
 import Types.HelperTypes exposing (DefaultLoginView(..), HttpRequestMethod(..), ProjectIdentifier)
 import Types.Msg exposing (Msg(..), ProjectSpecificMsgConstructor(..))
+import Types.OuterModel exposing (OuterModel)
 import Types.Project exposing (Project, ProjectSecret(..))
 import Types.Types
     exposing
@@ -32,7 +33,7 @@ import UUID
 import Url
 
 
-init : Flags -> ( Url.Url, Browser.Navigation.Key ) -> ( SharedModel, Cmd Msg )
+init : Flags -> ( Url.Url, Browser.Navigation.Key ) -> ( OuterModel, Cmd Msg )
 init flags urlKey =
     let
         currentTime =
@@ -81,9 +82,6 @@ init flags urlKey =
             , urlPathPrefix = flags.urlPathPrefix
             , navigationKey = Tuple.second urlKey
             , prevUrl = ""
-            , viewState =
-                -- This is will get replaced with the appropriate login view
-                NonProjectView LoginPicker
             , windowSize = { width = flags.width, height = flags.height }
             , unscopedProviders = []
             , projects = []
@@ -171,11 +169,10 @@ init flags urlKey =
         hydratedModel =
             LocalStorage.hydrateModelFromStoredState (emptyModel flags.showDebugMsgs) newClientUuid storedState
 
+        defaultViewState =
+            State.ViewState.defaultViewState hydratedModel
+
         viewState =
-            let
-                defaultViewState =
-                    State.ViewState.defaultViewState hydratedModel
-            in
             AppUrl.Parser.urlToViewState flags.urlPathPrefix defaultViewState (Tuple.first urlKey)
                 |> Maybe.withDefault (NonProjectView PageNotFound)
 
@@ -227,18 +224,26 @@ init flags urlKey =
                     )
                     ( hydratedModel, Cmd.none )
 
+        outerModel =
+            { sharedModel = requestResourcesModel, viewState = defaultViewState }
+
         ( setViewModel, setViewCmd ) =
             case viewState of
                 NonProjectView nonProjectViewConstructor ->
-                    setNonProjectView nonProjectViewConstructor requestResourcesModel
+                    setNonProjectView
+                        nonProjectViewConstructor
+                        outerModel
 
                 ProjectView projectId _ projectViewConstructor ->
                     -- If initial view is a project-specific view then we call setProjectView to fire any needed API calls
                     case GetterSetters.projectLookup requestResourcesModel projectId of
                         Just project ->
-                            setProjectView project projectViewConstructor requestResourcesModel
+                            setProjectView
+                                project
+                                projectViewConstructor
+                                outerModel
 
                         Nothing ->
-                            ( requestResourcesModel, Cmd.none )
+                            ( outerModel, Cmd.none )
     in
     ( setViewModel, Cmd.batch [ requestResourcesCmd, setViewCmd ] )

src/Exosphere.elm
@@ -5,7 +5,8 @@ import State.Init
 import State.State as State
 import State.Subscriptions
 import Types.Msg exposing (Msg(..))
-import Types.Types exposing (Flags, SharedModel)
+import Types.OuterModel exposing (OuterModel)
+import Types.Types exposing (Flags)
 import View.View exposing (view)
 
 
@@ -13,7 +14,7 @@ import View.View exposing (view)
 {- App Setup -}
 
 
-main : Program Flags SharedModel Msg
+main : Program Flags OuterModel Msg
 main =
     application
         { init = \flags url key -> State.Init.init flags ( url, key )

src/Rest/Neutron.elm
@@ -34,6 +34,7 @@ import Rest.Helpers
 import Types.Error exposing (ErrorContext, ErrorLevel(..))
 import Types.HelperTypes exposing (FloatingIpOption(..), HttpRequestMethod(..))
 import Types.Msg exposing (Msg(..), ProjectSpecificMsgConstructor(..), ServerSpecificMsgConstructor(..))
+import Types.OuterModel exposing (OuterModel)
 import Types.Project exposing (Project)
 import Types.Server exposing (NewServerNetworkOptions(..), Server, ServerOrigin(..))
 import Types.Types exposing (SharedModel)
@@ -436,20 +437,21 @@ requestCreateSecurityGroupRules project group rules errorMessage =
 {- HTTP Response Handling -}
 
 
-receiveNetworks : SharedModel -> Project -> List OSTypes.Network -> ( SharedModel, Cmd Msg )
-receiveNetworks model project networks =
+receiveNetworks : OuterModel -> Project -> List OSTypes.Network -> ( OuterModel, Cmd Msg )
+receiveNetworks outerModel project networks =
+    -- TODO this code should not care about view state
     let
         newProject =
             let
                 newNetsRDPP =
-                    RDPP.RemoteDataPlusPlus (RDPP.DoHave networks model.clientCurrentTime) (RDPP.NotLoading Nothing)
+                    RDPP.RemoteDataPlusPlus (RDPP.DoHave networks outerModel.sharedModel.clientCurrentTime) (RDPP.NotLoading Nothing)
             in
             { project | networks = newNetsRDPP }
 
         -- If we have a CreateServerRequest with no network UUID, populate it with a reasonable guess of a private network.
         -- Same comments above (in receiveFlavors) apply here.
         viewState =
-            case model.viewState of
+            case outerModel.viewState of
                 ProjectView _ viewParams projectViewConstructor ->
                     case projectViewConstructor of
                         CreateServer createServerViewParams ->
@@ -466,21 +468,21 @@ receiveNetworks model project networks =
                                             )
 
                                     _ ->
-                                        model.viewState
+                                        outerModel.viewState
 
                             else
-                                model.viewState
+                                outerModel.viewState
 
                         _ ->
-                            model.viewState
+                            outerModel.viewState
 
                 _ ->
-                    model.viewState
+                    outerModel.viewState
 
-        newModel =
-            GetterSetters.modelUpdateProject { model | viewState = viewState } newProject
+        newSharedModel =
+            GetterSetters.modelUpdateProject outerModel.sharedModel newProject
     in
-    ( newModel, Cmd.none )
+    ( { outerModel | viewState = viewState, sharedModel = newSharedModel }, Cmd.none )
 
 
 receiveFloatingIps : SharedModel -> Project -> List OSTypes.FloatingIp -> ( SharedModel, Cmd Msg )

src/Rest/Nova.elm
@@ -42,6 +42,7 @@ import Types.Error exposing (ErrorContext, ErrorLevel(..), HttpErrorWithBody)
 import Types.Guacamole as GuacTypes
 import Types.HelperTypes exposing (HttpRequestMethod(..), ProjectIdentifier, Url)
 import Types.Msg exposing (Msg(..), ProjectSpecificMsgConstructor(..), ServerSpecificMsgConstructor(..))
+import Types.OuterModel exposing (OuterModel)
 import Types.Project exposing (Project)
 import Types.Server exposing (ExoServerProps, NewServerNetworkOptions(..), Server, ServerOrigin(..))
 import Types.Types exposing (SharedModel)
@@ -863,8 +864,9 @@ receiveConsoleUrl model project server result =
             ( newModel, Cmd.none )
 
 
-receiveFlavors : SharedModel -> Project -> List OSTypes.Flavor -> ( SharedModel, Cmd Msg )
-receiveFlavors model project flavors =
+receiveFlavors : OuterModel -> Project -> List OSTypes.Flavor -> ( OuterModel, Cmd Msg )
+receiveFlavors outerModel project flavors =
+    -- TODO this code should not care about view state
     let
         newProject =
             { project | flavors = flavors }
@@ -876,7 +878,7 @@ receiveFlavors model project flavors =
         -- This could also benefit from some "railway-oriented programming" to avoid repetition of
         -- "otherwise just model.viewState" statments.
         viewState =
-            case model.viewState of
+            case outerModel.viewState of
                 ProjectView _ _ projectViewConstructor ->
                     case projectViewConstructor of
                         CreateServer viewParams ->
@@ -897,21 +899,21 @@ receiveFlavors model project flavors =
                                             )
 
                                     Nothing ->
-                                        model.viewState
+                                        outerModel.viewState
 
                             else
-                                model.viewState
+                                outerModel.viewState
 
                         _ ->
-                            model.viewState
+                            outerModel.viewState
 
                 _ ->
-                    model.viewState
+                    outerModel.viewState
 
-        newModel =
-            GetterSetters.modelUpdateProject { model | viewState = viewState } newProject
+        newSharedModel =
+            GetterSetters.modelUpdateProject outerModel.sharedModel newProject
     in
-    ( newModel, Cmd.none )
+    ( { outerModel | viewState = viewState, sharedModel = newSharedModel }, Cmd.none )
 
 
 receiveKeypairs : SharedModel -> Project -> List OSTypes.Keypair -> ( SharedModel, Cmd Msg )

src/State/Subscriptions.elm
@@ -3,13 +3,10 @@ module State.Subscriptions exposing (subscriptions)
 import Browser.Events
 import Time
 import Types.Msg exposing (Msg(..))
-import Types.Types
-    exposing
-        ( SharedModel
-        )
+import Types.OuterModel exposing (OuterModel)
 
 
-subscriptions : SharedModel -> Sub Msg
+subscriptions : OuterModel -> Sub Msg
 subscriptions _ =
     Sub.batch
         [ Time.every (5 * 1000) (Tick 5)

src/Types/OuterModel.elm
@@ -1,0 +1,10 @@
+module Types.OuterModel exposing (OuterModel)
+
+import Types.Types exposing (SharedModel)
+import Types.View exposing (ViewState)
+
+
+type alias OuterModel =
+    { sharedModel : SharedModel
+    , viewState : ViewState
+    }

commit 5fb4a4f0d84516980719b716130c15105f203f78

Fixing bug introduced in commit 5d5aa4f239d2b4ac620d570fa1f71289a7f259ae where setProjectView would not fire view-specific Cmds upon starting app, causing some resources to not display in UI

src/State/Init.elm
@@ -172,7 +172,7 @@ init flags urlKey =
         defaultViewState =
             State.ViewState.defaultViewState hydratedModel
 
-        viewState =
+        viewStateFromUrl =
             AppUrl.Parser.urlToViewState flags.urlPathPrefix defaultViewState (Tuple.first urlKey)
                 |> Maybe.withDefault (NonProjectView PageNotFound)
 
@@ -226,12 +226,14 @@ init flags urlKey =
 
         outerModel =
             { sharedModel = requestResourcesModel
-            , viewState = defaultViewState
+            , viewState =
+                -- This will get replaced with the appropriate view from viewStateFromUrl
+                NonProjectView LoginPicker
             , pendingCredentialedRequests = []
             }
 
         ( setViewModel, setViewCmd ) =
-            case viewState of
+            case viewStateFromUrl of
                 ExampleNestedView model ->
                     ( { outerModel | viewState = ExampleNestedView model }, Cmd.none )
 