332c292a15ed4d7fd920ec0bf309d2d0ad7e6b22
chris <chris@c-mart.in>
2021-08-23 16:46:52 -0700

    Using dropdown menus for floating IP assign Page

--- src/Page/FloatingIpAssign.elm
+++ src/Page/FloatingIpAssign.elm
@@ -2,12 +2,12 @@ module Page.FloatingIpAssign exposing (Model, Msg(..), init, update, view)
 
 import Element
 import Element.Font as Font
-import Element.Input as Input
 import Helpers.GetterSetters as GetterSetters
 import Helpers.RemoteDataPlusPlus as RDPP
 import Helpers.String
 import OpenStack.Types as OSTypes
 import Style.Helpers as SH
+import Style.Widgets.Select
 import Types.Project exposing (Project)
 import Types.SharedMsg as SharedMsg
 import View.Helpers as VH
@@ -68,8 +68,9 @@ view context project model =
                     )
                 |> List.map
                     (\s ->
-                        Input.option s.osProps.uuid
-                            (Element.text <| VH.possiblyUntitledResource s.osProps.name context.localization.virtualComputer)
+                        ( s.osProps.uuid
+                        , VH.possiblyUntitledResource s.osProps.name context.localization.virtualComputer
+                        )
                     )
 
         ipChoices =
@@ -81,10 +82,9 @@ view context project model =
                 |> List.filter (\ip -> GetterSetters.getFloatingIpServer project ip == Nothing)
                 |> List.map
                     (\ip ->
-                        Input.option ip.uuid
-                            (Element.el [ Font.family [ Font.monospace ] ] <|
-                                Element.text ip.address
-                            )
+                        ( ip.uuid
+                        , ip.address
+                        )
                     )
 
         selectIpText =
@@ -123,9 +123,8 @@ view context project model =
                         ]
 
               else
-                Input.radio []
-                    { label =
-                        Input.labelHidden selectServerText
+                Style.Widgets.Select.select []
+                    { label = selectServerText
                     , onChange = GotServerUuid
                     , options = serverChoices
                     , selected = model.serverUuid
@@ -144,8 +143,8 @@ view context project model =
                         ]
 
               else
-                Input.radio []
-                    { label = Input.labelHidden selectIpText
+                Style.Widgets.Select.select []
+                    { label = selectIpText
                     , onChange = GotIpUuid
                     , options = ipChoices
                     , selected = model.ipUuid
