d2ea4738ef2a79b7c966f5150dbcd12fe3b4ab36
chris <chris@c-mart.in>
2021-08-30 16:43:28 -0700

    implement image filtering logic

--- src/OpenStack/ImageFilter.elm
+++ src/OpenStack/ImageFilter.elm
@@ -9,3 +9,31 @@ type ImageFilter
     | ImageNameFilter String
     | ImageOsDistroFilter String
     | ImageOsVersionFilter String
+
+
+applyImageFilter : ImageFilter -> List OSTypes.Image -> List OSTypes.Image
+applyImageFilter filter images =
+    let
+        filterFunc : OSTypes.Image -> Bool
+        filterFunc image =
+            case filter of
+                ImageUuidFilter uuid ->
+                    image.uuid == uuid
+
+                ImageVisibilityFilter visibility ->
+                    image.visibility == visibility
+
+                ImageNameFilter name ->
+                    image.name == name
+
+                ImageOsDistroFilter osDistro ->
+                    image.osDistro
+                        |> Maybe.map (\osDistro_ -> osDistro_ == osDistro)
+                        |> Maybe.withDefault False
+
+                ImageOsVersionFilter osVersion ->
+                    image.osVersion
+                        |> Maybe.map (\osVersion_ -> osVersion_ == osVersion)
+                        |> Maybe.withDefault False
+    in
+    List.filter filterFunc images
