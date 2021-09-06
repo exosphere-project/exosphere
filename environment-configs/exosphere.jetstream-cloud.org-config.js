e6ac816aee800f09a0f31ca3ad1de98f256d06cc
chris <chris@c-mart.in>
2021-09-06 13:55:00 -0700

    Updating config JSONs with operating systems supported by Jetstream Clouds, formatted per RFC 8259

--- environment-configs/exosphere.jetstream-cloud.org-config.js
+++ environment-configs/exosphere.jetstream-cloud.org-config.js
@@ -1,67 +1,219 @@
 'use strict'
 
 var config = {
-  showDebugMsgs: false,
-  cloudCorsProxyUrl: "https://exosphere.jetstream-cloud.org/proxy",
-  urlPathPrefix: "exosphere",
-  palette: {primary: {r: 155, g: 33, b: 35}, secondary: {r: 52, g: 122, b: 140}},
-  logo: "assets/img/jetstream-logo-white.svg",
-  favicon: "assets/img/jetstream-favicon.ico",
-  appTitle: "Exosphere for Jetstream Cloud",
-  topBarShowAppTitle: false,
-  defaultLoginView: "jetstream",
-  aboutAppMarkdown: "This is the Exosphere interface for [Jetstream Cloud](https://jetstream-cloud.org), currently in beta. If you require assistance, please email help@jetstream-cloud.org and specify you are using Exosphere.\\\n\\\nUse of this site is subject to the Exosphere hosted sites [Privacy Policy](https://gitlab.com/exosphere/exosphere/-/blob/master/docs/privacy-policy.md) and [Acceptable Use Policy](https://gitlab.com/exosphere/exosphere/-/blob/master/docs/acceptable-use-policy.md).",
-  supportInfoMarkdown: "Please read about [using instances](https://iujetstream.atlassian.net/wiki/display/JWT/Jetstream+Public+Wiki) or [troubleshooting instances](https://wiki.jetstream-cloud.org/Troubleshooting+and+FAQ) for answers to common problems before submitting a request to support staff.",
-  userSupportEmail: "help@jetstream-cloud.org",
-  openIdConnectLoginConfig:
-    {
-      keystoneAuthUrl: "https://iu.jetstream-cloud.org:5000/v3",
-      webssoKeystoneEndpoint: "/auth/OS-FEDERATION/websso/openid?origin=https://exosphere.jetstream-cloud.org/exosphere/oidc-redirector",
-      oidcLoginIcon: "assets/img/XSEDE_Logo_Black_INF.png",
-      oidcLoginButtonLabel: "Add XSEDE Account",
-      oidcLoginButtonDescription: "Under construction, may not work, Jetstream IU Cloud only"
+  "showDebugMsgs":false,
+  "cloudCorsProxyUrl":"https://exosphere.jetstream-cloud.org/proxy",
+  "urlPathPrefix":"exosphere",
+  "palette":{
+    "primary":{
+      "r":155,
+      "g":33,
+      "b":35
     },
-  localization: {
-    openstackWithOwnKeystone: "cloud",
-    openstackSharingKeystoneWithAnother: "region",
-    unitOfTenancy: "allocation",
-    maxResourcesPerProject: "quota",
-    pkiPublicKeyForSsh: "SSH public key",
-    virtualComputer: "instance",
-    virtualComputerHardwareConfig: "flavor",
-    cloudInitData: "boot script",
-    commandDrivenTextInterface: "web shell",
-    staticRepresentationOfBlockDeviceContents: "image",
-    blockDevice: "volume",
-    nonFloatingIpAddress: "internal IP address",
-    floatingIpAddress: "public IP address",
-    publiclyRoutableIpAddress: "public IP address",
-    graphicalDesktopEnvironment: "web desktop"
+    "secondary":{
+      "r":52,
+      "g":122,
+      "b":140
+    }
   },
-  clouds: [
+  "logo":"assets/img/jetstream-logo-white.svg",
+  "favicon":"assets/img/jetstream-favicon.ico",
+  "appTitle":"Exosphere for Jetstream Cloud",
+  "topBarShowAppTitle":false,
+  "defaultLoginView":"jetstream",
+  "aboutAppMarkdown":"This is the Exosphere interface for [Jetstream Cloud](https://jetstream-cloud.org), currently in beta. If you require assistance, please email help@jetstream-cloud.org and specify you are using Exosphere.\\\n\\\nUse of this site is subject to the Exosphere hosted sites [Privacy Policy](https://gitlab.com/exosphere/exosphere/-/blob/master/docs/privacy-policy.md) and [Acceptable Use Policy](https://gitlab.com/exosphere/exosphere/-/blob/master/docs/acceptable-use-policy.md).",
+  "supportInfoMarkdown":"Please read about [using instances](https://iujetstream.atlassian.net/wiki/display/JWT/Jetstream+Public+Wiki) or [troubleshooting instances](https://wiki.jetstream-cloud.org/Troubleshooting+and+FAQ) for answers to common problems before submitting a request to support staff.",
+  "userSupportEmail":"help@jetstream-cloud.org",
+  "openIdConnectLoginConfig":{
+    "keystoneAuthUrl":"https://iu.jetstream-cloud.org:5000/v3",
+    "webssoKeystoneEndpoint":"/auth/OS-FEDERATION/websso/openid?origin=https://exosphere.jetstream-cloud.org/exosphere/oidc-redirector",
+    "oidcLoginIcon":"assets/img/XSEDE_Logo_Black_INF.png",
+    "oidcLoginButtonLabel":"Add XSEDE Account",
+    "oidcLoginButtonDescription":"Under construction, may not work, Jetstream IU Cloud only"
+  },
+  "localization":{
+    "openstackWithOwnKeystone":"cloud",
+    "openstackSharingKeystoneWithAnother":"region",
+    "unitOfTenancy":"allocation",
+    "maxResourcesPerProject":"quota",
+    "pkiPublicKeyForSsh":"SSH public key",
+    "virtualComputer":"instance",
+    "virtualComputerHardwareConfig":"flavor",
+    "cloudInitData":"boot script",
+    "commandDrivenTextInterface":"web shell",
+    "staticRepresentationOfBlockDeviceContents":"image",
+    "blockDevice":"volume",
+    "nonFloatingIpAddress":"internal IP address",
+    "floatingIpAddress":"public IP address",
+    "publiclyRoutableIpAddress":"public IP address",
+    "graphicalDesktopEnvironment":"web desktop"
+  },
+  "clouds":[
     {
-      keystoneHostname: "iu.jetstream-cloud.org",
-      userAppProxy: "proxy-j7m-iu.exosphere.app",
-      imageExcludeFilter: {
-        filterKey: "atmo_image_include",
-        filterValue: "true"
+      "keystoneHostname":"iu.jetstream-cloud.org",
+      "userAppProxy":"proxy-j7m-iu.exosphere.app",
+      "imageExcludeFilter":{
+        "filterKey":"atmo_image_include",
+        "filterValue":"true"
       },
-      featuredImageNamePrefix: "JS-API-Featured",
-      operatingSystemChoices: []
+      "featuredImageNamePrefix":"JS-API-Featured",
+      "operatingSystemChoices":[
+        {
+          "friendlyName":"Ubuntu",
+          "description":"Wide compatibility with community software packages, good choice for new users",
+          "logo":"assets/img/ubuntu.svg",
+          "versions":[
+            {
+              "friendlyName":"20.04 (latest)",
+              "isPrimary":true,
+              "filters":{
+                "name":"JS-API-Featured-Ubuntu20-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"20.04 with NVIDIA drivers",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-Ubuntu20-NVIDIA-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"18.04",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-Ubuntu18-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"18.04 with MATLAB",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-Ubuntu18-MATLAB-Latest",
+                "visibility":"public"
+              }
+            }
+          ]
+        },
+        {
+          "friendlyName":"CentOS",
+          "description":"Based on Red Hat Enterprise Linux (RHEL), compatible with RPM-based software",
+          "logo":"assets/img/centos.svg",
+          "versions":[
+            {
+              "friendlyName":"8 (latest)",
+              "isPrimary":true,
+              "filters":{
+                "name":"JS-API-Featured-CentOS8-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"7",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-CentOS7-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"7 with NVIDIA drivers",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-CentOS7-NVIDIA-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"7 with Intel compiler",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-CentOS7-Intel-Developer-Latest",
+                "visibility":"public"
+              }
+            }
+          ]
+        }
+      ]
     },
     {
-      keystoneHostname: "tacc.jetstream-cloud.org",
-      userAppProxy: "proxy-j7m-tacc.exosphere.app",
-      imageExcludeFilter: {
-        filterKey: "atmo_image_include",
-        filterValue: "true"
+      "keystoneHostname":"tacc.jetstream-cloud.org",
+      "userAppProxy":"proxy-j7m-tacc.exosphere.app",
+      "imageExcludeFilter":{
+        "filterKey":"atmo_image_include",
+        "filterValue":"true"
       },
-      featuredImageNamePrefix: "JS-API-Featured",
-      operatingSystemChoices: []
+      "featuredImageNamePrefix":"JS-API-Featured",
+      "operatingSystemChoices":[
+        {
+          "friendlyName":"Ubuntu",
+          "description":"Wide compatibility with community software packages, good choice for new users",
+          "logo":"assets/img/ubuntu.svg",
+          "versions":[
+            {
+              "friendlyName":"20.04 (latest)",
+              "isPrimary":true,
+              "filters":{
+                "name":"JS-API-Featured-Ubuntu20-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"18.04",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-Ubuntu18-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"16.04 with MATLAB",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-Ubuntu16-MATLAB-Latest",
+                "visibility":"public"
+              }
+            }
+          ]
+        },
+        {
+          "friendlyName":"CentOS",
+          "description":"Based on Red Hat Enterprise Linux (RHEL), compatible with RPM-based software",
+          "logo":"assets/img/centos.svg",
+          "versions":[
+            {
+              "friendlyName":"8 (latest)",
+              "isPrimary":true,
+              "filters":{
+                "name":"JS-API-Featured-CentOS8-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"7",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-CentOS7-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"7 with Intel compiler",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-CentOS7-Intel-Developer-Latest",
+                "visibility":"public"
+              }
+            }
+          ]
+        }
+      ]
     }
   ],
-  instanceConfigMgtRepoUrl: null,
-  instanceConfigMgtRepoCheckout: null
+  "instanceConfigMgtRepoUrl":null,
+  "instanceConfigMgtRepoCheckout":null
 }
 
 /* Matomo tracking code */
--- environment-configs/try-dev.exosphere.app-config.js
+++ environment-configs/try-dev.exosphere.app-config.js
@@ -1,51 +1,192 @@
 'use strict'
 
 var config = {
-  showDebugMsgs: false,
-  cloudCorsProxyUrl: "https://try-dev.exosphere.app/proxy",
-  urlPathPrefix: "exosphere",
-  palette: null,
-  logo: null,
-  favicon: null,
-  appTitle: null,
-  topBarShowAppTitle: true,
-  defaultLoginView: null,
-  aboutAppMarkdown: "Exosphere is a user-friendly, extensible client for cloud computing. Check out our [README on GitLab](https://gitlab.com/exosphere/exosphere/blob/master/README.md). To ask for help, report a bug, or request a new feature, [create an issue](https://gitlab.com/exosphere/exosphere/issues) on Exosphere's GitLab project. Someone will respond within a day or so. For real-time assistance, try Exosphere chat. Our chat is on [Gitter](https://gitter.im/exosphere-app/community) and [Matrix via Element](https://riot.im/app/#/room/#exosphere:matrix.org). The chat is bridged across both platforms, so join whichever you prefer.\\\n\\\nUse of this site is subject to the Exosphere hosted sites [Privacy Policy](https://gitlab.com/exosphere/exosphere/-/blob/master/docs/privacy-policy.md) and [Acceptable Use Policy](https://gitlab.com/exosphere/exosphere/-/blob/master/docs/acceptable-use-policy.md).",
-  supportInfoMarkdown: null,
-  userSupportEmail: "incoming+exosphere-exosphere-6891229-issue-@incoming.gitlab.com",
-  openIdConnectLoginConfig:
+  "showDebugMsgs":false,
+  "cloudCorsProxyUrl":"https://try-dev.exosphere.app/proxy",
+  "urlPathPrefix":"exosphere",
+  "palette":null,
+  "logo":null,
+  "favicon":null,
+  "appTitle":null,
+  "topBarShowAppTitle":true,
+  "defaultLoginView":null,
+  "aboutAppMarkdown":"Exosphere is a user-friendly, extensible client for cloud computing. Check out our [README on GitLab](https://gitlab.com/exosphere/exosphere/blob/master/README.md). To ask for help, report a bug, or request a new feature, [create an issue](https://gitlab.com/exosphere/exosphere/issues) on Exosphere's GitLab project. Someone will respond within a day or so. For real-time assistance, try Exosphere chat. Our chat is on [Gitter](https://gitter.im/exosphere-app/community) and [Matrix via Element](https://riot.im/app/#/room/#exosphere:matrix.org). The chat is bridged across both platforms, so join whichever you prefer.\\\n\\\nUse of this site is subject to the Exosphere hosted sites [Privacy Policy](https://gitlab.com/exosphere/exosphere/-/blob/master/docs/privacy-policy.md) and [Acceptable Use Policy](https://gitlab.com/exosphere/exosphere/-/blob/master/docs/acceptable-use-policy.md).",
+  "supportInfoMarkdown":null,
+  "userSupportEmail":"incoming+exosphere-exosphere-6891229-issue-@incoming.gitlab.com",
+  "openIdConnectLoginConfig":{
+    "keystoneAuthUrl":"https://iu.jetstream-cloud.org:5000/v3",
+    "webssoKeystoneEndpoint":"/auth/OS-FEDERATION/websso/openid?origin=https://try-dev.exosphere.app/exosphere/oidc-redirector",
+    "oidcLoginIcon":"assets/img/XSEDE_Logo_Black_INF.png",
+    "oidcLoginButtonLabel":"Add XSEDE Account",
+    "oidcLoginButtonDescription":"Under construction, may not work, Jetstream IU Cloud only"
+  },
+  "localization":null,
+  "clouds":[
     {
-      keystoneAuthUrl: "https://iu.jetstream-cloud.org:5000/v3",
-      webssoKeystoneEndpoint: "/auth/OS-FEDERATION/websso/openid?origin=https://try-dev.exosphere.app/exosphere/oidc-redirector",
-      oidcLoginIcon: "assets/img/XSEDE_Logo_Black_INF.png",
-      oidcLoginButtonLabel: "Add XSEDE Account",
-      oidcLoginButtonDescription: "Under construction, may not work, Jetstream IU Cloud only"
-    },
-  localization: null,
-  clouds: [
-    {
-      keystoneHostname: "iu.jetstream-cloud.org",
-      userAppProxy: "proxy-j7m-iu.exosphere.app",
-      imageExcludeFilter: {
-        filterKey: "atmo_image_include",
-        filterValue: "true"
+      "keystoneHostname":"iu.jetstream-cloud.org",
+      "userAppProxy":"proxy-j7m-iu.exosphere.app",
+      "imageExcludeFilter":{
+        "filterKey":"atmo_image_include",
+        "filterValue":"true"
       },
-      featuredImageNamePrefix: "JS-API-Featured",
-      operatingSystemChoices: []
+      "featuredImageNamePrefix":"JS-API-Featured",
+      "operatingSystemChoices":[
+        {
+          "friendlyName":"Ubuntu",
+          "description":"Wide compatibility with community software packages, good choice for new users",
+          "logo":"assets/img/ubuntu.svg",
+          "versions":[
+            {
+              "friendlyName":"20.04 (latest)",
+              "isPrimary":true,
+              "filters":{
+                "name":"JS-API-Featured-Ubuntu20-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"20.04 with NVIDIA drivers",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-Ubuntu20-NVIDIA-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"18.04",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-Ubuntu18-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"18.04 with MATLAB",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-Ubuntu18-MATLAB-Latest",
+                "visibility":"public"
+              }
+            }
+          ]
+        },
+        {
+          "friendlyName":"CentOS",
+          "description":"Based on Red Hat Enterprise Linux (RHEL), compatible with RPM-based software",
+          "logo":"assets/img/centos.svg",
+          "versions":[
+            {
+              "friendlyName":"8 (latest)",
+              "isPrimary":true,
+              "filters":{
+                "name":"JS-API-Featured-CentOS8-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"7",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-CentOS7-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"7 with NVIDIA drivers",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-CentOS7-NVIDIA-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"7 with Intel compiler",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-CentOS7-Intel-Developer-Latest",
+                "visibility":"public"
+              }
+            }
+          ]
+        }
+      ]
     },
     {
-      keystoneHostname: "tacc.jetstream-cloud.org",
-      userAppProxy: "proxy-j7m-tacc.exosphere.app",
-      imageExcludeFilter: {
-        filterKey: "atmo_image_include",
-        filterValue: "true"
+      "keystoneHostname":"tacc.jetstream-cloud.org",
+      "userAppProxy":"proxy-j7m-tacc.exosphere.app",
+      "imageExcludeFilter":{
+        "filterKey":"atmo_image_include",
+        "filterValue":"true"
       },
-      featuredImageNamePrefix: "JS-API-Featured",
-      operatingSystemChoices: []
+      "featuredImageNamePrefix":"JS-API-Featured",
+      "operatingSystemChoices":[
+        {
+          "friendlyName":"Ubuntu",
+          "description":"Wide compatibility with community software packages, good choice for new users",
+          "logo":"assets/img/ubuntu.svg",
+          "versions":[
+            {
+              "friendlyName":"20.04 (latest)",
+              "isPrimary":true,
+              "filters":{
+                "name":"JS-API-Featured-Ubuntu20-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"18.04",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-Ubuntu18-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"16.04 with MATLAB",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-Ubuntu16-MATLAB-Latest",
+                "visibility":"public"
+              }
+            }
+          ]
+        },
+        {
+          "friendlyName":"CentOS",
+          "description":"Based on Red Hat Enterprise Linux (RHEL), compatible with RPM-based software",
+          "logo":"assets/img/centos.svg",
+          "versions":[
+            {
+              "friendlyName":"8 (latest)",
+              "isPrimary":true,
+              "filters":{
+                "name":"JS-API-Featured-CentOS8-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"7",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-CentOS7-Latest",
+                "visibility":"public"
+              }
+            },
+            {
+              "friendlyName":"7 with Intel compiler",
+              "isPrimary":false,
+              "filters":{
+                "name":"JS-API-Featured-CentOS7-Intel-Developer-Latest",
+                "visibility":"public"
+              }
+            }
+          ]
+        }
+      ]
     }
   ],
-  instanceConfigMgtRepoUrl: null,
-  instanceConfigMgtRepoCheckout: "dev"
+  "instanceConfigMgtRepoUrl":null,
+  "instanceConfigMgtRepoCheckout":"dev"
 }
 
 /* Matomo tracking code */
