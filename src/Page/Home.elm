6b003f907fa5a2826091dc21c5d38bcee670c889
chris <chris@c-mart.in>
2021-10-05 17:44:30 -0700

    Breaking view out into a couple of helper functions, depending whether we have projects to show

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -57,40 +57,49 @@ view context sharedModel _ =
                 |> Set.fromList
                 |> Set.toList
     in
-    case sharedModel.projects of
-        [] ->
-            Element.column
-                [ Element.padding 10, Element.spacing 24, Element.centerX ]
-                [ Element.text <|
-                    String.join " "
-                        [ "You are not logged into any"
-                        , context.localization.unitOfTenancy
-                            |> Helpers.String.pluralize
-                        , "yet."
-                        ]
-                , Element.link
-                    [ Element.centerX ]
-                    { url =
-                        Route.toUrl context.urlPathPrefix
-                            (Route.defaultLoginPage
-                                sharedModel.style.defaultLoginView
-                            )
-                    , label =
-                        Widget.textButton
-                            (SH.materialStyle context.palette).button
-                            { text = "Add " ++ context.localization.unitOfTenancy
-                            , onPress = Just <| NoOp
-                            }
-                    }
-                ]
+    if List.isEmpty sharedModel.projects then
+        viewWithoutProjects context sharedModel
 
-        _ ->
-            Element.column [ Element.padding 10, Element.spacing 10, Element.width Element.fill ]
-                [ Element.el (VH.heading2 context.palette) <| Element.text "Clouds"
-                , Element.column
-                    [ Element.padding 10, Element.spacingXY 0 60 ]
-                    (List.map (renderCloud context sharedModel) uniqueKeystoneHostnames)
+    else
+        viewWithProjects context sharedModel uniqueKeystoneHostnames
+
+
+viewWithoutProjects : View.Types.Context -> SharedModel -> Element.Element Msg
+viewWithoutProjects context sharedModel =
+    Element.column
+        [ Element.padding 10, Element.spacing 24, Element.centerX ]
+        [ Element.text <|
+            String.join " "
+                [ "You are not logged into any"
+                , context.localization.unitOfTenancy
+                    |> Helpers.String.pluralize
+                , "yet."
                 ]
+        , Element.link
+            [ Element.centerX ]
+            { url =
+                Route.toUrl context.urlPathPrefix
+                    (Route.defaultLoginPage
+                        sharedModel.style.defaultLoginView
+                    )
+            , label =
+                Widget.textButton
+                    (SH.materialStyle context.palette).button
+                    { text = "Add " ++ context.localization.unitOfTenancy
+                    , onPress = Just NoOp
+                    }
+            }
+        ]
+
+
+viewWithProjects : View.Types.Context -> SharedModel -> List HelperTypes.KeystoneHostname -> Element.Element Msg
+viewWithProjects context sharedModel uniqueKeystoneHostnames =
+    Element.column [ Element.padding 10, Element.spacing 10, Element.width Element.fill ]
+        [ Element.el (VH.heading2 context.palette) <| Element.text "Clouds"
+        , Element.column
+            [ Element.padding 10, Element.spacingXY 0 60 ]
+            (List.map (renderCloud context sharedModel) uniqueKeystoneHostnames)
+        ]
 
 
 renderCloud : View.Types.Context -> SharedModel -> HelperTypes.KeystoneHostname -> Element.Element Msg
