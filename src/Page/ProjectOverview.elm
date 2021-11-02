0c1748ece6bb94f363be9a9d177ac6da93461f1f
chris <chris@c-mart.in>
2021-11-02 15:35:43 -0700

    servers tile on project overview page

--- src/Page/ProjectOverview.elm
+++ src/Page/ProjectOverview.elm
@@ -6,12 +6,14 @@ import Element.Font as Font
 import FeatherIcons
 import Helpers.RemoteDataPlusPlus as RDPP
 import Helpers.String
+import OpenStack.Types as OSTypes
 import Page.QuotaUsage
 import Route
 import Style.Helpers as SH
 import Style.Widgets.Card
 import Style.Widgets.Icon as Icon
 import Types.Project exposing (Project)
+import Types.Server exposing (Server)
 import Types.SharedMsg as SharedMsg
 import View.Helpers as VH
 import View.Types
@@ -38,18 +40,19 @@ update msg _ model =
 
 
 view : View.Types.Context -> Project -> Model -> Element.Element Msg
-view context p _ =
+view context project _ =
     let
         renderTile : Element.Element Msg -> String -> Route.ProjectRouteConstructor -> Element.Element Msg -> Element.Element Msg -> Element.Element Msg
         renderTile icon str projRouteConstructor quotaMeter contents =
             Element.link []
-                { url = Route.toUrl context.urlPathPrefix (Route.ProjectRoute p.auth.project.uuid projRouteConstructor)
+                { url = Route.toUrl context.urlPathPrefix (Route.ProjectRoute project.auth.project.uuid projRouteConstructor)
                 , label =
                     tile context
-                        [ Element.column [ Element.padding 10, Element.width Element.fill ]
+                        [ Element.column [ Element.padding 10, Element.width Element.fill, Element.spacing 20 ]
                             [ Element.row
                                 (VH.heading3 context.palette
                                     ++ [ Element.spacing 12
+                                       , Element.paddingEach { bottom = 0, left = 0, right = 0, top = 0 }
                                        , Border.width 0
                                        , Element.mouseOver
                                             [ Font.color
@@ -75,14 +78,14 @@ view context p _ =
                 Element.paragraph [ Font.italic ] [ Element.text description ]
 
         floatingIpsUsedCount =
-            p.floatingIps
+            project.floatingIps
                 -- Defaulting to 0 if not loaded yet, not the greatest factoring
                 |> RDPP.withDefault []
                 |> List.length
     in
     Element.column
         [ Element.spacing 15, Element.width Element.fill ]
-        [ VH.renderMaybe p.description renderDescription
+        [ VH.renderMaybe project.description renderDescription
         , Element.wrappedRow [ Element.spacing 25 ]
             [ renderTile
                 (FeatherIcons.server
@@ -95,11 +98,8 @@ view context p _ =
                     |> Helpers.String.toTitleCase
                 )
                 Route.ServerList
-                (Page.QuotaUsage.view context Page.QuotaUsage.Brief (Page.QuotaUsage.Compute p.computeQuota))
-                (Element.column [ Element.width Element.fill ]
-                    [ Element.text "some contents"
-                    ]
-                )
+                (Page.QuotaUsage.view context Page.QuotaUsage.Brief (Page.QuotaUsage.Compute project.computeQuota))
+                (serverTileContents context project)
             , renderTile
                 (FeatherIcons.hardDrive
                     |> FeatherIcons.toHtml []
@@ -111,7 +111,7 @@ view context p _ =
                     |> Helpers.String.toTitleCase
                 )
                 Route.VolumeList
-                (Page.QuotaUsage.view context Page.QuotaUsage.Brief (Page.QuotaUsage.Volume p.volumeQuota))
+                (Page.QuotaUsage.view context Page.QuotaUsage.Brief (Page.QuotaUsage.Volume project.volumeQuota))
                 (Element.column []
                     [ Element.text "some contents"
                     ]
@@ -123,7 +123,7 @@ view context p _ =
                     |> Helpers.String.toTitleCase
                 )
                 Route.FloatingIpList
-                (Page.QuotaUsage.view context Page.QuotaUsage.Brief (Page.QuotaUsage.FloatingIp p.computeQuota floatingIpsUsedCount))
+                (Page.QuotaUsage.view context Page.QuotaUsage.Brief (Page.QuotaUsage.FloatingIp project.computeQuota floatingIpsUsedCount))
                 (Element.column []
                     [ Element.text "some contents"
                     ]
@@ -145,6 +145,72 @@ view context p _ =
         ]
 
 
+serverTileContents : View.Types.Context -> Project -> Element.Element Msg
+serverTileContents context project =
+    let
+        ownServer : OSTypes.UserUuid -> Server -> Bool
+        ownServer userUuid server =
+            server.osProps.details.userUuid == userUuid
+
+        shownServers =
+            project.servers
+                |> RDPP.withDefault []
+                |> List.filter (ownServer project.auth.user.uuid)
+                |> List.take 5
+
+        numOtherServers =
+            List.length (RDPP.withDefault [] project.servers) - List.length shownServers
+
+        renderServer : Server -> Element.Element Msg
+        renderServer server =
+            Element.row [ Element.width Element.fill, Element.height (Element.px 30), Element.spacing 10 ]
+                [ VH.serverStatusBadge context.palette server
+                , VH.possiblyUntitledResource server.osProps.name context.localization.virtualComputer
+                    |> VH.ellipsizedText
+                ]
+    in
+    Element.column [ Element.width Element.fill, Element.spacing 15 ] <|
+        -- TODO render no resource
+        -- TODO deal with situation where there are no servers created by user but there are some created by other users
+        List.concat
+            [ if List.isEmpty shownServers then
+                [ mutedText context <|
+                    String.join " "
+                        [ "No"
+                        , context.localization.virtualComputer |> Helpers.String.pluralize
+                        , "created by you"
+                        ]
+                ]
+
+              else
+                List.map renderServer shownServers
+            , if numOtherServers > 0 then
+                [ mutedText context <|
+                    String.join " "
+                        [ "and"
+                        , String.fromInt numOtherServers
+                        , "more"
+                        , context.localization.virtualComputer |> Helpers.String.pluralize
+                        ]
+                ]
+
+              else
+                []
+            ]
+
+
+mutedText : View.Types.Context -> String -> Element.Element Msg
+mutedText context text =
+    Element.el
+        [ Element.centerX
+        , context.palette.muted
+            |> SH.toElementColor
+            |> Font.color
+        ]
+    <|
+        Element.text text
+
+
 tile : View.Types.Context -> List (Element.Element Msg) -> Element.Element Msg
 tile context contents =
-    Style.Widgets.Card.exoCardFixedSize context.palette 400 400 contents
+    Style.Widgets.Card.exoCardFixedSize context.palette 450 350 contents
