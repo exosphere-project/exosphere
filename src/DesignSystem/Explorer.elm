5284918dedb37e03150c57fb3b8539b55a235f42
Kyle Tee <tee.kyle@gmail.com>
2022-02-15 11:01:23 +0200

    feat: add icon stories & palette to design system #658

--- src/DesignSystem/Explorer.elm
+++ src/DesignSystem/Explorer.elm
@@ -2,6 +2,9 @@ module DesignSystem.Explorer exposing (main)
 
 import Element exposing (text)
 import Html
+import Style.Helpers as SH
+import Style.Types
+import Style.Widgets.Icon exposing (bell, console, copyToClipboard, history, ipAddress, lock, lockOpen, plusCircle, remove, roundRect, timesCircle)
 import UIExplorer
     exposing
         ( UIExplorerProgram
@@ -17,8 +20,29 @@ import UIExplorer
 --- elm-ui helpers
 
 
-toHtml element msg =
-    Element.layout [] <| element msg
+toHtml : Element.Element msg -> Html.Html msg
+toHtml a =
+    Element.layout [] <| a
+
+
+
+--- theme
+
+
+palette : Style.Types.ExoPalette
+palette =
+    SH.toExoPalette
+        Style.Types.defaultColors
+        { theme = Style.Types.Override Style.Types.Light, systemPreference = Nothing }
+
+
+
+--- styling
+
+
+defaultIcon : (Element.Color -> number -> icon) -> icon
+defaultIcon icon =
+    icon (palette.on.background |> SH.toElementColor) 25
 
 
 
@@ -33,7 +57,21 @@ main =
             |> category "Atoms"
                 [ storiesOf
                     "Text"
-                    [ ( "Default", \_ -> toHtml text "Connect to...", {} )
+                    [ ( "Default", \_ -> toHtml <| Element.text "Connect to...", {} )
+                    ]
+                , storiesOf
+                    "Icon"
+                    [ ( "bell", \_ -> toHtml <| defaultIcon <| bell, {} )
+                    , ( "console", \_ -> toHtml <| defaultIcon <| console, {} )
+                    , ( "copyToClipboard", \_ -> toHtml <| defaultIcon <| copyToClipboard, {} )
+                    , ( "history", \_ -> toHtml <| defaultIcon <| history, {} )
+                    , ( "ipAddress", \_ -> toHtml <| defaultIcon <| ipAddress, {} )
+                    , ( "lock", \_ -> toHtml <| defaultIcon <| lock, {} )
+                    , ( "lockOpen", \_ -> toHtml <| defaultIcon <| lockOpen, {} )
+                    , ( "plusCircle", \_ -> toHtml <| defaultIcon <| plusCircle, {} )
+                    , ( "remove", \_ -> toHtml <| defaultIcon <| remove, {} )
+                    , ( "roundRect", \_ -> toHtml <| defaultIcon <| roundRect, {} )
+                    , ( "timesCircle", \_ -> toHtml <| defaultIcon <| timesCircle, {} )
                     ]
                 ]
             |> category "Molecules"
