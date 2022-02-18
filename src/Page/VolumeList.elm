d79cdd99be657a3206558feed74be77f40732477
Jaladh Singhal <jaladhsinghal@gmail.com>
2022-02-18 16:15:13 +0530

    Add DataLaist with minimal functionality in VolumeList

--- src/Page/VolumeList.elm
+++ src/Page/VolumeList.elm
@@ -1,35 +1,43 @@
 module Page.VolumeList exposing (Model, Msg, init, update, view)
 
 import Element
+import Element.Font as Font
 import FeatherIcons
 import Helpers.GetterSetters as GetterSetters
 import Helpers.String
 import OpenStack.Types as OSTypes
 import Page.QuotaUsage
 import Page.VolumeDetail
+import Route
 import Set
+import Style.Helpers as SH
 import Style.Widgets.Card as ExoCard
+import Style.Widgets.DataList as DataList
+import Time
 import Types.Project exposing (Project)
 import Types.SharedMsg as SharedMsg exposing (ProjectSpecificMsgConstructor(..), SharedMsg(..))
 import View.Helpers as VH
 import View.Types
+import Widget
 
 
 type alias Model =
     { showHeading : Bool
     , expandedVols : Set.Set OSTypes.VolumeUuid
     , deleteConfirmations : Set.Set OSTypes.VolumeUuid
+    , dataListModel : DataList.Model
     }
 
 
 type Msg
     = GotExpandCard OSTypes.VolumeUuid Bool
     | VolumeDetailMsg OSTypes.VolumeUuid Page.VolumeDetail.Msg
+    | DataListMsg DataList.Msg
 
 
 init : Bool -> Model
 init showHeading =
-    Model showHeading Set.empty Set.empty
+    Model showHeading Set.empty Set.empty (DataList.init <| DataList.getDefaultFilterOptions [])
 
 
 update : Msg -> Project -> Model -> ( Model, Cmd Msg, SharedMsg.SharedMsg )
@@ -85,23 +93,31 @@ update msg project model =
                 Page.VolumeDetail.NoOp ->
                     ( model, Cmd.none, SharedMsg.NoOp )
 
+        DataListMsg dataListMsg ->
+            ( { model
+                | dataListModel =
+                    DataList.update dataListMsg model.dataListModel
+              }
+            , Cmd.none
+            , SharedMsg.NoOp
+            )
+
 
 view : View.Types.Context -> Project -> Model -> Element.Element Msg
 view context project model =
     let
         renderSuccessCase : List OSTypes.Volume -> Element.Element Msg
         renderSuccessCase volumes_ =
-            Element.column
-                (VH.exoColumnAttributes
-                    ++ [ Element.paddingXY 10 0
-                       , Element.spacing 15
-                       , Element.width Element.fill
-                       ]
-                )
-                (List.map
-                    (renderVolumeCard context project model)
-                    volumes_
-                )
+            -- TODO: use datalist instead
+            DataList.view
+                model.dataListModel
+                DataListMsg
+                context.palette
+                []
+                (volumeView model context project)
+                (volumeRecords volumes_)
+                []
+                []
     in
     Element.column
         [ Element.spacing 20, Element.width Element.fill ]
@@ -145,3 +161,122 @@ renderVolumeCard context project model volume =
             { showHeading = False, volumeUuid = volume.uuid, deleteConfirmations = model.deleteConfirmations }
             |> Element.map (VolumeDetailMsg volume.uuid)
         )
+
+
+type alias VolumeRecord =
+    DataList.DataRecord
+        { name : OSTypes.VolumeName
+        , size : OSTypes.VolumeSize
+        , status : OSTypes.VolumeStatus
+        , attachment : Maybe OSTypes.VolumeAttachment
+
+        -- TODO: figure how to fetch
+        -- , creationTime : Time.Posix
+        -- , creator : String
+        }
+
+
+volumeRecords : List OSTypes.Volume -> List VolumeRecord
+volumeRecords volumes =
+    List.map
+        (\volume ->
+            { id = volume.uuid
+            , selectable = False
+            , name = volume.name
+            , size = volume.size
+            , status = volume.status
+            , attachment = List.head volume.attachments
+            }
+        )
+        volumes
+
+
+volumeView :
+    Model
+    -> View.Types.Context
+    -> Project
+    -> VolumeRecord
+    -> Element.Element msg
+volumeView model context project volumeRecord =
+    let
+        volumeLink =
+            Element.link []
+                { url =
+                    Route.toUrl context.urlPathPrefix
+                        (Route.ProjectRoute (GetterSetters.projectIdentifier project) <|
+                            Route.VolumeDetail volumeRecord.id
+                        )
+                , label =
+                    Element.el
+                        [ Font.size 18
+                        , Font.color (SH.toElementColor context.palette.primary)
+                        ]
+                        (Element.text volumeRecord.name)
+                }
+
+        volumeAttachment =
+            let
+                serverName serverUuid =
+                    case GetterSetters.serverLookup project serverUuid of
+                        Just server ->
+                            server.osProps.name
+
+                        Nothing ->
+                            "unresolvable " ++ context.localization.virtualComputer ++ " name"
+            in
+            Element.row [ Element.alignRight ]
+                [ Element.text "Attached to "
+                , case volumeRecord.attachment of
+                    Just volumeAttachment_ ->
+                        Element.link []
+                            { url =
+                                Route.toUrl context.urlPathPrefix
+                                    (Route.ProjectRoute (GetterSetters.projectIdentifier project) <|
+                                        Route.ServerDetail volumeAttachment_.serverUuid
+                                    )
+                            , label =
+                                Element.el
+                                    [ Font.color (SH.toElementColor context.palette.primary)
+                                    ]
+                                    (Element.text <| serverName volumeAttachment_.serverUuid)
+                            }
+
+                    Nothing ->
+                        Element.text <| "No " ++ context.localization.virtualComputer
+                ]
+
+        volumeActions =
+            -- TODO: Add no action, detach, attach + delete for 3 possible cases
+            Element.text "Action"
+    in
+    Element.column
+        [ Element.spacing 12
+        , Element.width Element.fill
+        , Font.color (SH.toElementColorWithOpacity context.palette.on.background 0.62)
+        ]
+        [ Element.row [ Element.spacing 10, Element.width Element.fill ]
+            [ volumeLink
+            , volumeAttachment
+            ]
+        , Element.row
+            [ Element.spacing 8
+            , Element.width Element.fill
+            ]
+            [ Element.el [] (Element.text <| String.fromInt volumeRecord.size ++ " GB")
+
+            -- , Element.text "·"
+            -- , Element.paragraph []
+            --     [ Element.text "created "
+            --     , Element.el [ Font.color (SH.toElementColor context.palette.on.background) ]
+            --         (Element.text <|
+            --             DateFormat.Relative.relativeTime currentTime
+            --                 serverRecord.creationTime
+            --         )
+            --     , Element.text " by "
+            --     , Element.el [ Font.color (SH.toElementColor context.palette.on.background) ]
+            --         (Element.text serverRecord.creator)
+            --     ]
+            , Element.el [ Element.alignRight ]
+                volumeActions
+            ]
+        ]
