3e98e1e2331169f179d43fd55a1e76a6f25c7dce
Jaladh Singhal <jaladhsinghal@gmail.com>
2022-04-19 17:01:22 +0530

    Move popover style & attribs from Style.Helpers to Popover

--- src/Style/Widgets/Popover.elm
+++ src/Style/Widgets/Popover.elm
@@ -1,21 +1,154 @@
-module Style.Widgets.Popover exposing (popover)
+module Style.Widgets.Popover exposing (popover, popoverAttribs, popoverStyleDefaults)
 
 import Element
+import Element.Background as Background
+import Element.Border as Border
 import Html.Attributes
 import Html.Events
 import Set
 import Style.Helpers as SH
-import Style.Types as ST
+import Style.Types as ST exposing (ExoPalette, PopoverPosition(..))
 import Types.SharedMsg
 import View.Types
 
 
+popoverStyleDefaults : ExoPalette -> List (Element.Attribute msg)
+popoverStyleDefaults palette =
+    [ Element.padding 10
+    , Background.color <| SH.toElementColor palette.background
+    , Border.width 1
+    , Border.color <| SH.toElementColorWithOpacity palette.on.background 0.16
+    , Border.shadow SH.shadowDefaults
+    ]
 
--- We need unique id for each popover because we need to check if a click target was descendant of a specific popover
--- TODO: remove `popoverId` as parameter - consumer shouldn't bother about it.
--- Generate a unique id in widget itself, something like CopyableText but there's no string to hash
+
+popoverAttribs :
+    Element.Element msg
+    -> PopoverPosition
+    -> Maybe Int
+    -> List (Element.Attribute msg)
+popoverAttribs popoverContent position distanceToTarget =
+    let
+        padding =
+            Maybe.withDefault 6 distanceToTarget
+
+        alignOnYAttribs percentStr =
+            -- alignment on Y axis of a nearby element doesn't work without this
+            [ Element.htmlAttribute <| Html.Attributes.style "top" percentStr
+            , Element.htmlAttribute <| Html.Attributes.style "transform" ("translateY(-" ++ percentStr ++ ")")
+            ]
+
+        attribs :
+            { nearbyElement : Element.Element msg -> Element.Attribute msg
+            , alignment : Element.Attribute msg
+            , onLeftOrRight : Bool
+            , additional : List (Element.Attribute msg)
+            }
+        attribs =
+            case position of
+                PositionTopLeft ->
+                    { nearbyElement = Element.above
+                    , alignment = Element.alignLeft
+                    , onLeftOrRight = False
+                    , additional = []
+                    }
+
+                PositionTop ->
+                    { nearbyElement = Element.above
+                    , alignment = Element.centerX
+                    , onLeftOrRight = False
+                    , additional = []
+                    }
+
+                PositionTopRight ->
+                    { nearbyElement = Element.above
+                    , alignment = Element.alignRight
+                    , onLeftOrRight = False
+                    , additional = []
+                    }
+
+                PositionRightTop ->
+                    { nearbyElement = Element.onRight
+                    , alignment = Element.alignTop
+                    , onLeftOrRight = True
+                    , additional = alignOnYAttribs "0%"
+                    }
+
+                PositionRight ->
+                    { nearbyElement = Element.onRight
+                    , alignment = Element.centerY
+                    , onLeftOrRight = True
+                    , additional = alignOnYAttribs "50%"
+                    }
+
+                PositionRightBottom ->
+                    { nearbyElement = Element.onRight
+                    , alignment = Element.alignBottom
+                    , onLeftOrRight = True
+                    , additional = alignOnYAttribs "100%"
+                    }
+
+                PositionBottomRight ->
+                    { nearbyElement = Element.below
+                    , alignment = Element.alignRight
+                    , onLeftOrRight = False
+                    , additional = []
+                    }
+
+                PositionBottom ->
+                    { nearbyElement = Element.below
+                    , alignment = Element.centerX
+                    , onLeftOrRight = False
+                    , additional = []
+                    }
+
+                PositionBottomLeft ->
+                    { nearbyElement = Element.below
+                    , alignment = Element.alignLeft
+                    , onLeftOrRight = False
+                    , additional = []
+                    }
+
+                PositionLeftBottom ->
+                    { nearbyElement = Element.onLeft
+                    , alignment = Element.alignBottom
+                    , onLeftOrRight = True
+                    , additional = alignOnYAttribs "100%"
+                    }
+
+                PositionLeft ->
+                    { nearbyElement = Element.onLeft
+                    , alignment = Element.centerY
+                    , onLeftOrRight = True
+                    , additional = alignOnYAttribs "50%"
+                    }
+
+                PositionLeftTop ->
+                    { nearbyElement = Element.onLeft
+                    , alignment = Element.alignTop
+                    , onLeftOrRight = True
+                    , additional = alignOnYAttribs "0%"
+                    }
+    in
+    [ attribs.nearbyElement <|
+        Element.el
+            ([ attribs.alignment
+             , if attribs.onLeftOrRight then
+                Element.paddingXY padding 0
+
+               else
+                Element.paddingXY 0 padding
+             ]
+                ++ attribs.additional
+            )
+            popoverContent
+    ]
 
 
+{-| We need unique id for each popover because we need to check if a click target was descendant of a specific popover
+TODO: remove `popoverId` as parameter - consumer shouldn't bother about it.
+Generate a unique id in widget itself, something like CopyableText but there's no string to hash
+-}
 popover :
     View.Types.Context
     -> (Types.SharedMsg.SharedMsg -> msg)
@@ -45,9 +178,9 @@ popover context sharedMsgMapper { id, content, contentStyleAttrs, position, dist
     Element.el
         ((Element.htmlAttribute <| Html.Attributes.id id)
             :: (if popoverIsShown then
-                    SH.popoverAttribs
+                    popoverAttribs
                         (Element.el
-                            (SH.popoverStyleDefaults context.palette
+                            (popoverStyleDefaults context.palette
                                 -- Add or override default style with passed style attributes
                                 ++ contentStyleAttrs
                             )
