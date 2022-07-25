9816d43630164249ac10805152808af9a9361199
Kyle Tee <tee.kyle@gmail.com>
2022-07-25 16:36:47 +0200

    feat: enrich the rendering of cards in the design system

--- src/DesignSystem/Stories/Card.elm
+++ src/DesignSystem/Stories/Card.elm
@@ -2,6 +2,7 @@ module DesignSystem.Stories.Card exposing (ExpandoCardState, stories)
 
 import DesignSystem.Helpers exposing (Plugins, Renderer, ThemeModel, palettize)
 import Element
+import FeatherIcons
 import Style.Widgets.Button as Button
 import Style.Widgets.Card exposing (clickableCardFixedSize, exoCard, exoCardWithTitleAndSubtitle, expandoCard)
 import Style.Widgets.CopyableText exposing (copyableText)
@@ -40,10 +41,28 @@ stories renderer { onPress, onExpand } =
         [ ( "exoCard"
           , \m ->
                 renderer (palettize m) <|
-                    exoCard (palettize m)
-                        (Element.column VH.exoColumnAttributes
-                            [ Text.mono "192.168.1.1" ]
-                        )
+                    Element.column []
+                        [ exoCard (palettize m)
+                            (Element.column
+                                VH.contentContainer
+                                [ Text.subheading (palettize m)
+                                    []
+                                    Element.none
+                                    "Status"
+                                , Element.row []
+                                    [ Element.el [] (Element.text <| "Available") ]
+                                , VH.compactKVRow "UUID:" <| copyableText (palettize m) [] "3050e6a0-10e2-4ecf-97c5-d197995ac621"
+                                , VH.compactKVRow
+                                    (String.concat
+                                        [ "Created from "
+                                        , "image"
+                                        , ":"
+                                        ]
+                                    )
+                                    (Element.text "JS-API-Featured-Ubuntu16-MATLAB-Latest")
+                                ]
+                            )
+                        ]
           , { note = note }
           )
         , ( "clickable card with fixed size"
@@ -51,46 +70,62 @@ stories renderer { onPress, onExpand } =
                 renderer (palettize m) <|
                     Element.link []
                         { url = "/#Organisms/Card"
-                        , label = clickableCardFixedSize (palettize m) 300 300 [ Text.body "Lorem ipsum dolor sit amet." ]
+                        , label =
+                            clickableCardFixedSize (palettize m)
+                                300
+                                200
+                                [ Text.body "Project TNT"
+                                , Element.column
+                                    [ Element.height (Element.px 120), Element.padding 10, Element.spacing 12 ]
+                                    [ Element.row [ Element.spacing 8 ]
+                                        [ FeatherIcons.server |> FeatherIcons.toHtml [] |> Element.html |> Element.el []
+                                        , Text.body "6 instances"
+                                        ]
+                                    ]
+                                ]
                         }
           , { note = note }
           )
         , ( "exoCard with title & subtitle"
           , \m ->
                 renderer (palettize m) <|
-                    exoCardWithTitleAndSubtitle (palettize m)
-                        (Style.Widgets.CopyableText.copyableText
-                            (palettize m)
-                            [ Text.monoFontFamily ]
-                            "192.168.1.1"
-                        )
-                        (Button.default
-                            (palettize m)
-                            { text = "Unassign"
-                            , onPress = onPress
-                            }
-                        )
-                        (Text.body "Assigned to a resource that Exosphere cannot represent")
+                    Element.column []
+                        [ exoCardWithTitleAndSubtitle (palettize m)
+                            (copyableText
+                                (palettize m)
+                                [ Text.monoFontFamily ]
+                                "192.168.1.1"
+                            )
+                            (Button.default
+                                (palettize m)
+                                { text = "Unassign"
+                                , onPress = onPress
+                                }
+                            )
+                            (Text.body "Assigned to a resource that Exosphere cannot represent")
+                        ]
           , { note = note }
           )
         , ( "expandoCard"
           , \m ->
                 renderer (palettize m) <|
-                    expandoCard (palettize m)
-                        m.customModel.expandoCard.expanded
-                        onExpand
-                        (Text.body "Backup SSD")
-                        (Text.body "25 GB")
-                        (Element.column
-                            VH.contentContainer
-                            [ VH.compactKVRow "Name:" <| Text.body "Backup SSD"
-                            , VH.compactKVRow "Status:" <| Text.body "Ready"
-                            , VH.compactKVRow "Description:" <|
-                                Text.p [ Element.width Element.fill ] <|
-                                    [ Element.text "Solid State Drive" ]
-                            , VH.compactKVRow "UUID:" <| copyableText (palettize m) [] "6205e1a8-9a5d-4325-bb0d-219f09a4d988"
-                            ]
-                        )
+                    Element.column []
+                        [ expandoCard (palettize m)
+                            m.customModel.expandoCard.expanded
+                            onExpand
+                            (Text.body "Backup SSD")
+                            (Text.body "25 GB")
+                            (Element.column
+                                VH.contentContainer
+                                [ VH.compactKVRow "Name:" <| Text.body "Backup SSD"
+                                , VH.compactKVRow "Status:" <| Text.body "Ready"
+                                , VH.compactKVRow "Description:" <|
+                                    Text.p [ Element.width Element.fill ] <|
+                                        [ Element.text "Solid State Drive" ]
+                                , VH.compactKVRow "UUID:" <| copyableText (palettize m) [] "6205e1a8-9a5d-4325-bb0d-219f09a4d988"
+                                ]
+                            )
+                        ]
           , { note = note }
           )
         ]
