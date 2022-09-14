78c48e2301080c57f7514bce7daf36ed5dddefcb
Jaladh Singhal <jaladhsinghal@gmail.com>
2022-09-14 21:33:26 +0530

    Added spacer in Style Helpers

--- src/Style/Helpers.elm
+++ src/Style/Helpers.elm
@@ -3,6 +3,7 @@ module Style.Helpers exposing
     , dropdownItemStyle
     , materialStyle
     , shadowDefaults
+    , spacer
     , toCssColor
     , toElementColor
     , toElementColorWithOpacity
@@ -425,4 +426,32 @@ dropdownItemStyle palette =
                    , Element.height Element.shrink
                    ]
         , labelRow = textButtonDefaults.labelRow ++ [ Element.spacing 12 ]
+    }
+
+
+{-| Fixed amount of space between elements that should be used to maintain consistency.
+
+In elm-ui Element.padding and Element.spacing fields of this record should be used instead of hardcoded numbers.
+The sizes chosen in this record are based on <https://www.refactoringui.com/> book's page number 73 (establishing spacing and sizing system).
+
+-}
+spacer :
+    { px4 : Int
+    , px8 : Int
+    , px12 : Int
+    , px16 : Int
+    , px24 : Int
+    , px32 : Int
+    , px48 : Int
+    , px64 : Int
+    }
+spacer =
+    { px4 = 4
+    , px8 = 8
+    , px12 = 12
+    , px16 = 16
+    , px24 = 24
+    , px32 = 32
+    , px48 = 48
+    , px64 = 64
     }
