286cf65fa9e3cad827877ed0a1ffa41d79170cbf
Michael White <michael.p.m.white@gmail.com>
2024-02-02 16:19:02 -0700

    Avoid "boolean blindness" when checking if a node is in a popover

--- src/Style/Widgets/Popover/Types.elm
+++ src/Style/Widgets/Popover/Types.elm
@@ -1,4 +1,4 @@
-module Style.Widgets.Popover.Types exposing (PopoverId)
+module Style.Widgets.Popover.Types exposing (NodePositionRelativeToPopover(..), PopoverId)
 
 {-| This will be used as HTML id attribute so it must be unique.
 Using name/purpose of popover is generally enough to avoid collisions.
@@ -7,3 +7,8 @@ Using name/purpose of popover is generally enough to avoid collisions.
 
 type alias PopoverId =
     String
+
+
+type NodePositionRelativeToPopover
+    = NodeInside
+    | NodeOutside
