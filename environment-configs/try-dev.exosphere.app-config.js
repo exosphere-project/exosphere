0cd7c571dc26d3ab85721b25c6a3005e0a47c8ac
Franklyn Tackitt <git@frank.af>
2024-03-05 12:19:28 -0700

    Add typescript types.d.ts for config.js and cloud_configs.js

--- environment-configs/try-dev.exosphere.app-config.js
+++ environment-configs/try-dev.exosphere.app-config.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/** @type {Exosphere.Configuration} */
 var config = {
   showDebugMsgs: false,
   cloudCorsProxyUrl: "https://try-dev.exosphere.app/proxy",
@@ -51,5 +52,6 @@ _paq.push(["enableLinkTracking"]);
   g.type = "text/javascript";
   g.async = true;
   g.src = u + "matomo.js";
+  // @ts-ignore
   s.parentNode.insertBefore(g, s);
 })();
---
+++ types.d.ts
@@ -0,0 +1,147 @@
+declare namespace Exosphere {
+  type RGB = {
+    r: number;
+    g: number;
+    b: number;
+  };
+
+  type Palette = {
+    primary: RGB;
+    secondary: RGB;
+  };
+
+  type Theme = {
+    light: Palette;
+    dark: Palette;
+  };
+
+  type SentryConfig = {
+    dsnPublicKey: string;
+    dsnHost: string;
+    dsnProjectId: string;
+    releaseVersion: string;
+    environmentName: string;
+  };
+
+  type OpenIdConnectLoginConfig = {
+    keystoneAuthUrl: string;
+    webssoKeystoneEndpoint: string;
+    oidcLoginIcon: string;
+    oidcLoginButtonLabel: string;
+    oidcLoginButtonDescription: string;
+  };
+
+  type Localization = {
+    openstackWithOwnKeystone: "cloud" | string;
+    openstackSharingKeystoneWithAnother: "region" | string;
+    unitOfTenancy: "project" | string;
+    maxResourcesPerProject: "resource limit" | string;
+    pkiPublicKeyForSsh: "SSH public key" | string;
+    virtualComputer: "instance" | string;
+    virtualComputerHardwareConfig: "size" | string;
+    cloudInitData: "boot script" | string;
+    commandDrivenTextInterface: "terminal" | string;
+    staticRepresentationOfBlockDeviceContents: "image" | string;
+    blockDevice: "volume" | string;
+    share: "share" | string;
+    accessRule: "access rule" | string;
+    exportLocation: "export location" | string;
+    nonFloatingIpAddress: "internal IP address" | string;
+    floatingIpAddress: "floating IP address" | string;
+    publiclyRoutableIpAddress: "public IP address" | string;
+    graphicalDesktopEnvironment: "graphical desktop" | string;
+    hostname: "hostname" | string;
+  };
+
+  type CloudUserApplicationProxy = {
+    region: null | string;
+    hostname: string;
+  };
+
+  type CloudMetadataFilter = {
+    filterKey: string;
+    filterValue: string;
+  };
+
+  type CloudInstanceType = {
+    friendlyName: string;
+    description: string;
+    logo: string;
+    versions: Array<{
+      friendlyName: string;
+      isPrimary: boolean;
+      imageFilters: {
+        name?: string;
+        uuid?: string;
+        visibility?: "private" | "shared" | "community" | "public";
+        osDistro?: string;
+        osVersion?: string;
+        metadata?: CloudMetadataFilter;
+      };
+      restrictFlavorIds: null | Array<string>;
+    }>;
+  };
+
+  type ServerActions =
+    | "Confirm"
+    | "Revert"
+    | "Lock"
+    | "Unlock"
+    | "Start"
+    | "Unpause"
+    | "Resume"
+    | "Unshelve"
+    | "Suspend"
+    | "Shelve"
+    | "Resize"
+    | "Reboot"
+    | "Delete"
+    | "Pause"
+    | "Stop";
+
+  type CloudFlavorGroup = {
+    matchOn: string;
+    title: string;
+    description: null | string;
+    disallowedActions: Array<ServerActions | string>;
+  };
+
+  type CloudConfig = {
+    keystoneHostname: string;
+    friendlyName: string;
+    userAppProxy: null | Array<CloudUserApplicationProxy>;
+
+    imageExcludeFilter: null | CloudMetadataFilter;
+    featuredImageNamePrefix: null | string;
+    instanceTypes: Array<CloudInstanceType>;
+    flavorGroups: Array<CloudFlavorGroup>;
+    desktopMessage: null | string;
+  };
+
+  type CloudConfigs = {
+    clouds: Array<CloudConfig>;
+  };
+
+  type Configuration = {
+    showDebugMsgs: boolean;
+    cloudCorsProxyUrl: null | string;
+    urlPathPrefix: null | string;
+    palette: null | Theme;
+    topBarShowAppTitle: boolean;
+
+    appTitle: null | string;
+    logo: null | string;
+
+    favicon: null | string;
+    defaultLoginView: null | string;
+    aboutAppMarkdown: null | string;
+    supportInfoMarkdown: null | string;
+    userSupportEmailAddress: null | string;
+    userSupportEmailSubject: null | string;
+    instanceConfigMgtRepoUrl: null | string;
+    instanceConfigMgtRepoCheckout: null | string;
+    sentryConfig: null | SentryConfig;
+    openIdConnectLoginConfig: null | OpenIdConnectLoginConfig;
+    localization: null | Localization;
+  };
+}
