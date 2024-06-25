ca430c817288eaad972af9877e3235a981ad9479
Kyle Tee <tee.kyle@gmail.com>
2024-06-25 14:07:47 +0200

    refactor: tag widget to add other colours

--- src/Style/Widgets/Tag.elm
+++ src/Style/Widgets/Tag.elm
@@ -1,5 +1,6 @@
-module Style.Widgets.Tag exposing (tag)
+module Style.Widgets.Tag exposing (tag, tagPositive, tagWithColor)
 
+import Color
 import Element
 import Element.Background as Background
 import Element.Border as Border
@@ -10,15 +11,25 @@ import Style.Widgets.Spacer exposing (spacer)
 import Style.Widgets.Text as Text
 
 
-tag : Style.Types.ExoPalette -> String -> Element.Element msg
-tag palette text =
+tagWithColor : Color.Color -> String -> Element.Element msg
+tagWithColor color text =
     Element.el
-        [ Background.color (SH.toElementColorWithOpacity palette.primary 0.1)
+        [ Background.color (SH.toElementColorWithOpacity color 0.1)
         , Border.width 1
-        , Border.color (SH.toElementColorWithOpacity palette.primary 0.7)
+        , Border.color (SH.toElementColorWithOpacity color 0.7)
         , Text.fontSize Text.Tiny
-        , Font.color (SH.toElementColor palette.primary)
+        , Font.color (SH.toElementColor color)
         , Border.rounded 20
         , Element.paddingXY spacer.px8 spacer.px4
         ]
         (Element.text text)
+
+
+tag : Style.Types.ExoPalette -> String -> Element.Element msg
+tag palette text =
+    tagWithColor palette.primary text
+
+
+tagPositive : Style.Types.ExoPalette -> String -> Element.Element msg
+tagPositive palette text =
+    tagWithColor palette.success.textOnNeutralBG text
