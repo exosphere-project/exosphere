d6f83441dfe97be80ce3dc2b3b0575efbd7bf570
chris <chris@c-mart.in>
2024-10-24 13:14:56 -0700

    convert cloud-init user data to a MIME multi-part file
    This change moves the bash commands outside of the cloud config data's YAML structure. This will make it easier to use bash functions and loops, to implement retry logic.
    
    Per https://cloudinit.readthedocs.io/en/latest/explanation/format.html#mime-multi-part-archive

--- src/ServerDeploy.elm
+++ src/ServerDeploy.elm
@@ -7,7 +7,47 @@ cloudInitUserDataTemplate =
        The virtualenv case expression is due to CentOS 7 requiring use of `virtualenv-3`,
        Ubuntu 18 requiring `python3 -m virtualenv`, and everything else just using `virtualenv`.
     -}
-    """#cloud-config
+    """Content-Type: multipart/mixed; boundary="===============2389165605550749110=="
+MIME-Version: 1.0
+Number-Attachments: 2
+
+--===============2389165605550749110==
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="part-001"
+
+#!/bin/bash
+
+echo on > /proc/sys/kernel/printk_devkmsg || true  # Disable console rate limiting for distros that use kmsg
+sleep 1  # Ensures that console log output from any previous command completes before the following command begins
+echo '{"status":"running", "epoch": '$(date '+%s')'000}' | tee --append /dev/console > /dev/kmsg || true
+chmod 640 /var/log/cloud-init-output.log
+{create-cluster-command}
+(which apt-get && apt-get install -y python3-venv) # Install python3-venv on Debian-based platforms
+(which yum     && yum     install -y python3)      # Install python3 on RHEL-based platforms
+python3 -m venv /opt/ansible-venv
+. /opt/ansible-venv/bin/activate
+pip install --upgrade pip
+pip install ansible-core passlib
+ansible-pull \\
+  --url "{instance-config-mgt-repo-url}" \\
+  --checkout "{instance-config-mgt-repo-checkout}" \\
+  --directory /opt/instance-config-mgt \\
+  -i /opt/instance-config-mgt/ansible/hosts \\
+  -e "{ansible-extra-vars}" \\
+  /opt/instance-config-mgt/ansible/playbook.yml
+ANSIBLE_RETURN_CODE=$?
+if [ $ANSIBLE_RETURN_CODE -eq 0 ]; then STATUS="complete"; else STATUS="error"; fi
+sleep 1  # Ensures that console log output from any previous commands complete before the following command begins
+echo '{"status":"'$STATUS'", "epoch": '$(date '+%s')'000}' | tee --append /dev/console > /dev/kmsg || true
+
+--===============2389165605550749110==
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="part-002"
+
 users:
   - default
   - name: exouser
@@ -19,30 +59,6 @@ package_update: true
 package_upgrade: {install-os-updates}
 packages:
   - git{write-files}
-runcmd:
-  - echo on > /proc/sys/kernel/printk_devkmsg || true  # Disable console rate limiting for distros that use kmsg
-  - sleep 1  # Ensures that console log output from any previous command completes before the following command begins
-  - >-
-    echo '{"status":"running", "epoch": '$(date '+%s')'000}' | tee --append /dev/console > /dev/kmsg || true
-  - chmod 640 /var/log/cloud-init-output.log
-  - {create-cluster-command}
-  - (which apt-get && apt-get install -y python3-venv) # Install python3-venv on Debian-based platforms
-  - (which yum     && yum     install -y python3)      # Install python3 on RHEL-based platforms
-  - |-
-    python3 -m venv /opt/ansible-venv
-    . /opt/ansible-venv/bin/activate
-    pip install --upgrade pip
-    pip install ansible-core passlib
-    ansible-pull \\
-      --url "{instance-config-mgt-repo-url}" \\
-      --checkout "{instance-config-mgt-repo-checkout}" \\
-      --directory /opt/instance-config-mgt \\
-      -i /opt/instance-config-mgt/ansible/hosts \\
-      -e "{ansible-extra-vars}" \\
-      /opt/instance-config-mgt/ansible/playbook.yml
-  - ANSIBLE_RETURN_CODE=$?
-  - if [ $ANSIBLE_RETURN_CODE -eq 0 ]; then STATUS="complete"; else STATUS="error"; fi
-  - sleep 1  # Ensures that console log output from any previous commands complete before the following command begins
-  - >-
-    echo '{"status":"'$STATUS'", "epoch": '$(date '+%s')'000}' | tee --append /dev/console > /dev/kmsg || true
+
+--===============2389165605550749110==--
 """
