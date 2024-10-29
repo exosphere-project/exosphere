82569e02b6b39a4a69fc4fce9ee9b26a9a07f7ce
chris <chris@c-mart.in>
2024-10-28 17:37:21 -0700

    refactor: use PollInterval type to provide semantic meaning and keep poll intervals consistent

--- src/Orchestration/Helpers.elm
+++ src/Orchestration/Helpers.elm
@@ -4,6 +4,7 @@ import Helpers.GetterSetters as GetterSetters
 import Helpers.Helpers exposing (serverFromThisExoClient)
 import Helpers.RemoteDataPlusPlus as RDPP
 import OpenStack.Types as OSTypes
+import Orchestration.Types exposing (PollInterval(..))
 import Time
 import Types.Project exposing (Project)
 import Types.Server exposing (ExoSetupStatus(..), Server, ServerFromExoProps, ServerOrigin(..))
@@ -82,23 +83,44 @@ pollRDPP rdpp time pollIntervalMs =
     not receivedRecentlyEnough && not loading
 
 
+pollIntervalToMs : PollInterval -> Int
+pollIntervalToMs i =
+    case i of
+        Rapid ->
+            -- 15 seconds
+            15000
+
+        Regular ->
+            -- 2 minutes
+            120000
+
+        Seldom ->
+            -- 30 minutes
+            1800000
+
+
 serverPollIntervalMs : Project -> Server -> Int
 serverPollIntervalMs project server =
+    serverPollIntervalMs_ project server |> pollIntervalToMs
+
+
+serverPollIntervalMs_ : Project -> Server -> PollInterval
+serverPollIntervalMs_ project server =
     case GetterSetters.serverCreatedByCurrentUser project server.osProps.uuid of
         Just True ->
             myOwnServerPollIntervalMs server
 
         _ ->
-            1800000
+            Seldom
 
 
-myOwnServerPollIntervalMs : Server -> Int
+myOwnServerPollIntervalMs : Server -> PollInterval
 myOwnServerPollIntervalMs server =
     case
         server.osProps.details.openstackStatus
     of
         OSTypes.ServerBuild ->
-            15000
+            Rapid
 
         _ ->
             case
@@ -109,31 +131,31 @@ myOwnServerPollIntervalMs server =
             of
                 ( False, Nothing, ServerNotFromExo ) ->
                     -- Not created from Exosphere, not deleting or waiting a pending server action
-                    120000
+                    Regular
 
                 ( False, Nothing, ServerFromExo fromExoProps ) ->
                     myOwnServerFromExoPollIntervalMs fromExoProps
 
                 _ ->
                     -- We're expecting OpenStack status to change (or server to be deleted) very soon
-                    15000
+                    Rapid
 
 
-myOwnServerFromExoPollIntervalMs : ServerFromExoProps -> Int
+myOwnServerFromExoPollIntervalMs : ServerFromExoProps -> PollInterval
 myOwnServerFromExoPollIntervalMs { exoSetupStatus } =
     case exoSetupStatus.data of
         RDPP.DoHave ( ExoSetupWaiting, _ ) _ ->
             -- Exosphere-created, booting up for the first time
-            15000
+            Rapid
 
         RDPP.DoHave ( ExoSetupRunning, _ ) _ ->
             -- Exosphere-created, running setup
-            15000
+            Rapid
 
         RDPP.DoHave _ _ ->
             -- Exosphere-created, not waiting for setup to complete
-            120000
+            Regular
 
         RDPP.DontHave ->
             -- Exosphere-created and Exosphere setup status unknown
-            15000
+            Rapid
