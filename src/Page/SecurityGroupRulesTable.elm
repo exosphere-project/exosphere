4e6322a6290ddc1bb621c0ccd206f7091e6364d9
Kyle Tee <tee.kyle@gmail.com>
2024-11-28 16:26:46 +0200

    refactor: rules table to expose render remote

--- src/Page/SecurityGroupRulesTable.elm
+++ src/Page/SecurityGroupRulesTable.elm
@@ -1,4 +1,4 @@
-module Page.SecurityGroupRulesTable exposing (RulesTableRowCustomiser, defaultRowStyle, rulesTable, rulesTableWithRowCustomiser, view)
+module Page.SecurityGroupRulesTable exposing (RulesTableRowCustomiser, defaultRowStyle, renderRemote, rulesTable, rulesTableWithRowCustomiser, view)
 
 import Element
 import Element.Font as Font
@@ -47,6 +47,59 @@ type alias RulesTableRowCustomiser msg =
         , styleForRow : List (Element.Attribute msg)
         , rightElementForRow : Maybe (Element.Element msg)
         }
+
+
+renderRemote :
+    View.Types.Context
+    -> ProjectIdentifier
+    -> (SecurityGroupUuid -> Maybe SecurityGroup)
+    -> SecurityGroupRule
+    -> Element.Element msg
+renderRemote context projectId securityGroupForUuid rule =
+    case ( rule.remoteIpPrefix, rule.remoteGroupUuid ) of
+        -- Either IP prefix or remote security group.
+        ( Just ipPrefix, _ ) ->
+            Text.body <| ipPrefix
+
+        ( _, Just remoteGroupUuid ) ->
+            -- Look up a the remote security group locally.
+            case securityGroupForUuid remoteGroupUuid of
+                Just securityGroup ->
+                    Element.link []
+                        { url =
+                            Route.toUrl context.urlPathPrefix
+                                (Route.ProjectRoute projectId <|
+                                    Route.SecurityGroupDetail securityGroup.uuid
+                                )
+                        , label =
+                            Element.el
+                                [ Font.color (SH.toElementColor context.palette.primary) ]
+                                (Element.text <|
+                                    VH.extendedResourceName
+                                        (Just securityGroup.name)
+                                        securityGroup.uuid
+                                        context.localization.securityGroup
+                                )
+                        }
+
+                Nothing ->
+                    Text.body <|
+                        VH.extendedResourceName
+                            Nothing
+                            remoteGroupUuid
+                            context.localization.securityGroup
+
+        ( Nothing, Nothing ) ->
+            -- Assume 'any' address when neither remote group nor IP prefix are specified
+            case rule.ethertype of
+                Ipv4 ->
+                    Text.body "0.0.0.0/0"
+
+                Ipv6 ->
+                    Text.body "::/0"
+
+                _ ->
+                    Text.body "-"
 
 
 rulesTableWithRowCustomiser :
@@ -134,50 +187,7 @@ rulesTableWithRowCustomiser context projectId { rules, securityGroupForUuid } cu
                             \item ->
                                 let
                                     cell =
-                                        case ( item.remoteIpPrefix, item.remoteGroupUuid ) of
-                                            -- Either IP prefix or remote security group.
-                                            ( Just ipPrefix, _ ) ->
-                                                Text.body <| ipPrefix
-
-                                            ( _, Just remoteGroupUuid ) ->
-                                                -- Look up a the remote security group locally.
-                                                case securityGroupForUuid remoteGroupUuid of
-                                                    Just securityGroup ->
-                                                        Element.link []
-                                                            { url =
-                                                                Route.toUrl context.urlPathPrefix
-                                                                    (Route.ProjectRoute projectId <|
-                                                                        Route.SecurityGroupDetail securityGroup.uuid
-                                                                    )
-                                                            , label =
-                                                                Element.el
-                                                                    [ Font.color (SH.toElementColor context.palette.primary) ]
-                                                                    (Element.text <|
-                                                                        VH.extendedResourceName
-                                                                            (Just securityGroup.name)
-                                                                            securityGroup.uuid
-                                                                            context.localization.securityGroup
-                                                                    )
-                                                            }
-
-                                                    Nothing ->
-                                                        Text.body <|
-                                                            VH.extendedResourceName
-                                                                Nothing
-                                                                remoteGroupUuid
-                                                                context.localization.securityGroup
-
-                                            ( Nothing, Nothing ) ->
-                                                -- Assume 'any' address when neither remote group nor IP prefix are specified
-                                                case item.ethertype of
-                                                    Ipv4 ->
-                                                        Text.body "0.0.0.0/0"
-
-                                                    Ipv6 ->
-                                                        Text.body "::/0"
-
-                                                    _ ->
-                                                        Text.body "-"
+                                        renderRemote context projectId securityGroupForUuid item
                                 in
                                 container item <| cell
                       }
