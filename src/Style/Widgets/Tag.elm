e47be6e65f4ef295fad0e5e88d061eb39c5be89e
Kyle Tee <tee.kyle@gmail.com>
2025-02-28 15:48:23 +0200

    feat: add a warning tag widget

--- src/Style/Widgets/Tag.elm
+++ src/Style/Widgets/Tag.elm
@@ -1,4 +1,4 @@
-module Style.Widgets.Tag exposing (tag, tagNeutral, tagPositive, tagWithColor)
+module Style.Widgets.Tag exposing (tag, tagNeutral, tagPositive, tagWarning, tagWithColor)
 
 import Color
 import Element
@@ -38,3 +38,8 @@ tagPositive palette text =
 tagNeutral : Style.Types.ExoPalette -> String -> Element.Element msg
 tagNeutral palette text =
     tagWithColor palette.muted.textOnNeutralBG text
+
+
+tagWarning : Style.Types.ExoPalette -> String -> Element.Element msg
+tagWarning palette text =
+    tagWithColor palette.warning.textOnNeutralBG text
