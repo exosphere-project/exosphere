e9af543d64faf5cf201fbd98932c46fe822312c0
Kyle Tee <tee.kyle@gmail.com>
2025-07-25 14:47:04 +0200

    feat: add connectivity check helper

---
+++ src/Helpers/Connectivity.elm
@@ -0,0 +1,122 @@
+module Helpers.Connectivity exposing (ConnectionPorts(..), ConnectionRemote(..), ConnectivityRule, isConnectionPermitted, securityGroupRuleTemplateToConnectivtyRule)
+
+import Helpers.String exposing (removeEmptiness)
+import OpenStack.SecurityGroupRule exposing (Remote(..), SecurityGroupRule, SecurityGroupRuleDirection, SecurityGroupRuleEthertype, SecurityGroupRuleProtocol, SecurityGroupRuleTemplate, getRemote, portRangeSubsumedBy, protocolSubsumedBy, remoteMatch)
+
+
+type ConnectionRemote
+    = AllRemotes
+    | SpecificRemote Remote
+    | SomeRemote
+
+
+type ConnectionPorts
+    = AllPorts
+    | PortRange Int Int
+    | SomePort
+
+
+type alias ConnectivityRule =
+    { ethertype : SecurityGroupRuleEthertype
+    , direction : SecurityGroupRuleDirection
+    , protocol : Maybe SecurityGroupRuleProtocol
+    , ports : ConnectionPorts
+    , remote : ConnectionRemote
+    , description : Maybe String
+    }
+
+
+securityGroupRuleTemplateToConnectivtyRule : SecurityGroupRuleTemplate -> ConnectivityRule
+securityGroupRuleTemplateToConnectivtyRule { ethertype, direction, protocol, portRangeMin, portRangeMax, remoteIpPrefix, remoteGroupUuid, description } =
+    { ethertype = ethertype
+    , direction = direction
+    , protocol = protocol
+    , ports =
+        case ( portRangeMin, portRangeMax ) of
+            ( Just min, Just max ) ->
+                PortRange min max
+
+            ( Nothing, Nothing ) ->
+                -- Rules often don't have a port range & "at least some port" is a more intuitive connectivity conversion.
+                SomePort
+
+            -- These single unbounded ranges shouldn't occur in real life.
+            ( Just _, Nothing ) ->
+                SomePort
+
+            ( Nothing, Just _ ) ->
+                SomePort
+    , remote =
+        case ( remoteIpPrefix, remoteGroupUuid ) of
+            ( Just ipPrefix, Nothing ) ->
+                SpecificRemote (RemoteIpPrefix ipPrefix)
+
+            ( Nothing, Just groupUuid ) ->
+                SpecificRemote (RemoteGroupUuid groupUuid)
+
+            ( Nothing, Nothing ) ->
+                -- Rules often don't have a remote & "at least some remote" is a more intuitive connectivity conversion.
+                SomeRemote
+
+            ( Just _, Just _ ) ->
+                -- Should not happen since IP & remote group are mutually exclusive.
+                SomeRemote
+    , description = removeEmptiness description
+    }
+
+
+isConnectionPermitted : ConnectivityRule -> List SecurityGroupRule -> Bool
+isConnectionPermitted connection rules =
+    rules
+        |> List.any
+            (\rule ->
+                (rule.ethertype == connection.ethertype)
+                    && (rule.direction == connection.direction)
+                    && protocolSubsumedBy connection.protocol rule.protocol
+                    && portsPermittedBy connection.ports ( rule.portRangeMin, rule.portRangeMax )
+                    && remotePermittedBy connection.remote rule
+            )
+
+
+portsPermittedBy : ConnectionPorts -> ( Maybe Int, Maybe Int ) -> Bool
+portsPermittedBy connectionPorts ( portMin, portMax ) =
+    case ( connectionPorts, portMin, portMax ) of
+        ( PortRange min max, Just _, Just _ ) ->
+            portRangeSubsumedBy ( Just min, Just max ) ( portMin, portMax )
+
+        ( SomePort, Just _, Just _ ) ->
+            -- Any single port is permitted.
+            True
+
+        ( _, Nothing, Nothing ) ->
+            -- All ports are permitted.
+            True
+
+        ( AllPorts, Just 0, Just 65535 ) ->
+            True
+
+        _ ->
+            False
+
+
+remotePermittedBy : ConnectionRemote -> SecurityGroupRule -> Bool
+remotePermittedBy remoteA rule =
+    let
+        remoteB =
+            getRemote rule
+    in
+    case ( remoteA, remoteB ) of
+        ( _, Nothing ) ->
+            -- Rule allows all connections.
+            True
+
+        ( SomeRemote, Just _ ) ->
+            -- Any remote connectivity will do.
+            True
+
+        ( AllRemotes, Just _ ) ->
+            -- Rule is too narrow.
+            False
+
+        ( SpecificRemote a, Just b ) ->
+            remoteMatch a b
