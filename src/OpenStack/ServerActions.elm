eff7c1bd1017e1318424c6757fd7c31ca2348cf3
Kyle Tee <tee.kyle@gmail.com>
2026-03-13 17:02:41 +0200

    feat: add server action types

--- src/OpenStack/ServerActions.elm
+++ src/OpenStack/ServerActions.elm
@@ -1,5 +1,114 @@
-module OpenStack.ServerActions exposing (ServerActionName)
+module OpenStack.ServerActions exposing (ServerAction(..), ServerActionName, serverActionToString, stringToServerAction)
+
+
+type ServerAction
+    = ConfirmResize
+    | RevertResize
+    | Lock
+    | Unlock
+    | Start
+    | Stop
+    | Unpause
+    | Pause
+    | Resume
+    | Suspend
+    | Shelve
+    | Unshelve
+    | Reboot
+    | UnsupportedAction String
 
 
 type alias ServerActionName =
     String
+
+
+serverActionToString : ServerAction -> String
+serverActionToString serverAction =
+    case serverAction of
+        ConfirmResize ->
+            "confirmResize"
+
+        RevertResize ->
+            "revertResize"
+
+        Lock ->
+            "lock"
+
+        Unlock ->
+            "unlock"
+
+        Start ->
+            "os-start"
+
+        Stop ->
+            "os-stop"
+
+        Unpause ->
+            "unpause"
+
+        Pause ->
+            "pause"
+
+        Resume ->
+            "resume"
+
+        Suspend ->
+            "suspend"
+
+        Shelve ->
+            "shelve"
+
+        Unshelve ->
+            "unshelve"
+
+        Reboot ->
+            "reboot"
+
+        UnsupportedAction str ->
+            str
+
+
+stringToServerAction : String -> ServerAction
+stringToServerAction str =
+    case str of
+        "confirmResize" ->
+            ConfirmResize
+
+        "revertResize" ->
+            RevertResize
+
+        "lock" ->
+            Lock
+
+        "unlock" ->
+            Unlock
+
+        "os-start" ->
+            Start
+
+        "os-stop" ->
+            Stop
+
+        "unpause" ->
+            Unpause
+
+        "pause" ->
+            Pause
+
+        "resume" ->
+            Resume
+
+        "suspend" ->
+            Suspend
+
+        "shelve" ->
+            Shelve
+
+        "unshelve" ->
+            Unshelve
+
+        "reboot" ->
+            Reboot
+
+        _ ->
+            UnsupportedAction str
